ef3509b75b82ad51f4484545ce473af4dd5274d9
Dzmitry Kozel <dmitrokoz88@gmail.com>
2021-10-23 00:40:37 +0300

    BUILD SUCCESS

---
+++ src/main/java/core/basesyntax/Main.java
@@ -0,0 +1,13 @@
+package core.basesyntax;
+
+import core.basesyntax.strategy.DiscountService;
+
+public class Main {
+    public static void main(String[] args) {
+        DiscountStrategy discountStrategy = new DiscountStrategy();
+        DiscountService discountService =
+                discountStrategy.getDiscountServiceBySpecialEvent("Black Friday");
+        double discount = discountService.getDiscount();
+        System.out.println(discount);
+    }
+}
