a5e196ace50e2939f04ff2bc34f477a59a7be321
Yaroslav Kavun <yka40znp@gmail.com>
2021-10-24 18:23:13 +0300

    Implements getDiscountService... method using 'Strategy' pattern

---
+++ src/main/java/core/basesyntax/strategy/impl/NewYearDiscountService.java
@@ -0,0 +1,13 @@
+package core.basesyntax.strategy.impl;
+
+import core.basesyntax.strategy.DiscountService;
+
+public class NewYearDiscountService implements DiscountService {
+    private static final double DISCOUNT = 20.0;
+
+    @Override
+    public double getDiscount() {
+        return DISCOUNT;
+    }
+
+}
