123cc2ff2f0f3b4b612f074a8d956ff0608548c9
Yaroslav Yarmoshyk <yarmoshyk96@gmail.com>
2021-10-25 13:10:39 +0300

    added fixes after review

--- src/main/java/core/basesyntax/strategy/impl/NewYearDiscountService.java
+++ src/main/java/core/basesyntax/strategy/impl/NewYearDiscountService.java
@@ -3,8 +3,10 @@ package core.basesyntax.strategy.impl;
 import core.basesyntax.strategy.DiscountService;
 
 public class NewYearDiscountService implements DiscountService {
+    private static final double DISCOUNT = 20;
+
     @Override
     public double getDiscount() {
-        return 20;
+        return DISCOUNT;
     }
 }
