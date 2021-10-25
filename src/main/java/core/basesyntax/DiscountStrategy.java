838e4caa32ed9f73d3ea797d62e01a410812f04d
nishchenkokateryna <kateryna.nishchenko@gmail.com>
2021-10-25 20:41:44 +0300

    Added constants

--- src/main/java/core/basesyntax/DiscountStrategy.java
+++ src/main/java/core/basesyntax/DiscountStrategy.java
@@ -7,13 +7,17 @@ import core.basesyntax.strategy.impl.DefaultDiscountService;
 import core.basesyntax.strategy.impl.NewYearDiscountService;
 
 public class DiscountStrategy {
+    private static final String BIRTHDAY_DAY = "Birthday";
+    private static final String NEW_YEAR_DAY = "New Year";
+    private static final String BLACK_FRIDAY_DAY = "Black Friday";
+
     public DiscountService getDiscountServiceBySpecialEvent(String specialEvent) {
         switch (specialEvent) {
-            case "Birthday":
+            case BIRTHDAY_DAY:
                 return new BirthdayDiscountService();
-            case "Black Friday":
+            case BLACK_FRIDAY_DAY:
                 return new BlackFridayDiscountService();
-            case "New Year":
+            case NEW_YEAR_DAY:
                 return new NewYearDiscountService();
             default:
                 return new DefaultDiscountService();
