59903531403beb45365b1a5493c077a4a7edc391
Roman Polulikh <polilikh.r@gmail.com>
2021-10-26 14:48:11 +0300

    removed redundant constants

--- src/main/java/core/basesyntax/strategy/impl/DefaultDiscountService.java
+++ src/main/java/core/basesyntax/strategy/impl/DefaultDiscountService.java
@@ -3,10 +3,9 @@ package core.basesyntax.strategy.impl;
 import core.basesyntax.strategy.DiscountService;
 
 public class DefaultDiscountService implements DiscountService {
-    private static final double DISCOUNT = 0;
 
     @Override
     public double getDiscount() {
-        return DISCOUNT;
+        return 0;
     }
 }
