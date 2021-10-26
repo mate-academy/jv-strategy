696f5d07a038ce1486314d1296eb088257bc30ec
Volodimir <vova.stepanenko2020@gmail.com>
2021-10-26 12:10:41 +0300

    add access modifier

--- src/main/java/core/basesyntax/DiscountStrategy.java
+++ src/main/java/core/basesyntax/DiscountStrategy.java
@@ -7,7 +7,7 @@ import core.basesyntax.strategy.impl.DefaultDiscountService;
 import core.basesyntax.strategy.impl.NewYearDiscountService;
 
 public class DiscountStrategy {
-    DiscountService getDiscountServiceBySpecialEvent(String specialEvent) {
+    public DiscountService getDiscountServiceBySpecialEvent(String specialEvent) {
         switch (specialEvent) {
             case "New Year":
                 return new NewYearDiscountService();
