bb2a2ff9edeef779fd809382f8b4d638d7bbd04b
Alexsis Moskovchuk <alekssiuss@gmail.com>
2021-10-26 17:31:49 +0300

    cleanup

--- src/main/java/core/basesyntax/DiscountStrategy.java
+++ src/main/java/core/basesyntax/DiscountStrategy.java
@@ -7,16 +7,16 @@ import core.basesyntax.strategy.impl.DefaultDiscountService;
 import core.basesyntax.strategy.impl.NewYearDiscountService;
 
 public class DiscountStrategy {
-   public DiscountService getDiscountServiceBySpecialEvent(String specialEvent) {
-       switch (specialEvent) {
-           case "Birthday":
-               return new BirthdayDiscountService();
-           case "New Year":
-               return new NewYearDiscountService();
-           case "Black Friday":
-               return new BlackFridayDiscountService();
-           default:
-               return new DefaultDiscountService();
-       }
-   }
+    public DiscountService getDiscountServiceBySpecialEvent(String specialEvent) {
+        switch (specialEvent) {
+            case "Birthday":
+                return new BirthdayDiscountService();
+            case "New Year":
+                return new NewYearDiscountService();
+            case "Black Friday":
+                return new BlackFridayDiscountService();
+            default:
+                return new DefaultDiscountService();
+        }
+    }
 }
