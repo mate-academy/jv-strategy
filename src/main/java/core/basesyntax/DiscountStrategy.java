a4b48145be317b0ec65863f1a7ce610c14c44ce8
Yevheniy Savenkov <yevheniy.savenkov@gmail.com>
2021-10-27 16:29:51 +0300

    ашÐfifth commit, finally working, checkstye passed

--- src/main/java/core/basesyntax/DiscountStrategy.java
+++ src/main/java/core/basesyntax/DiscountStrategy.java
@@ -1,7 +1,10 @@
 package core.basesyntax;
 
-import core.basesyntax.strategy.impl.*;
 import core.basesyntax.strategy.DiscountService;
+import core.basesyntax.strategy.impl.BirthdayDiscountService;
+import core.basesyntax.strategy.impl.BlackFridayDiscountService;
+import core.basesyntax.strategy.impl.DefaultDiscountService;
+import core.basesyntax.strategy.impl.NewYearDiscountService;
 
 public class DiscountStrategy {
     private static final String EVENT_NEW_YEAR = "New Year";
