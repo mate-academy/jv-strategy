daeb2783ac72775ac2bf5c65f866846811833c96
Yevheniy Savenkov <yevheniy.savenkov@gmail.com>
2021-10-28 16:29:42 +0300

    sixth commit, redundant constants removed

--- src/main/java/core/basesyntax/DiscountStrategy.java
+++ src/main/java/core/basesyntax/DiscountStrategy.java
@@ -7,17 +7,13 @@ import core.basesyntax.strategy.impl.DefaultDiscountService;
 import core.basesyntax.strategy.impl.NewYearDiscountService;
 
 public class DiscountStrategy {
-    private static final String EVENT_NEW_YEAR = "New Year";
-    private static final String EVENT_BLACK_FRIDAY = "Black Friday";
-    private static final String EVENT_BIRTHDAY = "Birthday";
-
     public DiscountService getDiscountServiceBySpecialEvent(String specialEvent) {
         switch (specialEvent) {
-            case EVENT_NEW_YEAR:
+            case "New Year":
                 return new NewYearDiscountService();
-            case EVENT_BIRTHDAY:
+            case "Birthday":
                 return new BirthdayDiscountService();
-            case EVENT_BLACK_FRIDAY:
+            case "Black Friday":
                 return new BlackFridayDiscountService();
             default:
                 return new DefaultDiscountService();
--- src/main/java/core/basesyntax/strategy/impl/BirthdayDiscountService.java
+++ src/main/java/core/basesyntax/strategy/impl/BirthdayDiscountService.java
@@ -3,10 +3,8 @@ package core.basesyntax.strategy.impl;
 import core.basesyntax.strategy.DiscountService;
 
 public class BirthdayDiscountService implements DiscountService {
-    private static final double DISCOUNT = 33.0;
-
     @Override
     public double getDiscount() {
-        return DISCOUNT;
+        return 33.0;
     }
 }
