f6b0cab657250967a7c569ffcd73b53d2ef801cc
Andriy-dz <andriydzdz@gmail.com>
2021-10-28 16:55:07 +0300

    deleted empty line and added an access modifier

--- src/main/java/core/basesyntax/strategy/impl/BirthdayDiscountService.java
+++ src/main/java/core/basesyntax/strategy/impl/BirthdayDiscountService.java
@@ -4,6 +4,7 @@ import core.basesyntax.strategy.DiscountService;
 
 public class BirthdayDiscountService implements DiscountService {
     private static final double BIRTHDAY = 33.0;
+
     @Override
     public double getDiscount() {
         return BIRTHDAY;
--- src/main/java/core/basesyntax/strategy/impl/NewYearDiscountService.java
+++ src/main/java/core/basesyntax/strategy/impl/NewYearDiscountService.java
@@ -4,6 +4,7 @@ import core.basesyntax.strategy.DiscountService;
 
 public class NewYearDiscountService implements DiscountService {
     private static final double NEW_YEAR = 20.0;
+
     @Override
     public double getDiscount() {
         return NEW_YEAR;
