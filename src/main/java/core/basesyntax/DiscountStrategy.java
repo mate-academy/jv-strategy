30e2e5481f787eb10453b7f4cf38fbc52471f2e8
KostyNikiforov <www.kosta.kon@gmail.com>
2021-10-28 09:42:10 +0300

    Main class was removed. Package impl was moved to strategy.

--- src/main/java/core/basesyntax/DiscountStrategy.java
+++ src/main/java/core/basesyntax/DiscountStrategy.java
@@ -1,10 +1,10 @@
 package core.basesyntax;
 
-import core.basesyntax.impl.BirthdayDiscountService;
-import core.basesyntax.impl.BlackFridayDiscountService;
-import core.basesyntax.impl.DefaultDiscountService;
-import core.basesyntax.impl.NewYearDiscountService;
 import core.basesyntax.strategy.DiscountService;
+import core.basesyntax.strategy.impl.BirthdayDiscountService;
+import core.basesyntax.strategy.impl.BlackFridayDiscountService;
+import core.basesyntax.strategy.impl.DefaultDiscountService;
+import core.basesyntax.strategy.impl.NewYearDiscountService;
 
 public class DiscountStrategy {
     public DiscountService getDiscountServiceBySpecialEvent(String specialEvent) {
