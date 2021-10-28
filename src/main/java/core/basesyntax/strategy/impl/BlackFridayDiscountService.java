0f82cbd4abd5cba069441bd2ad79b88ca8b8b311
Oleksandr Ostapchuk <shurion.ua@gmail.com>
2021-10-28 17:56:44 +0300

    All constants was delete

--- src/main/java/core/basesyntax/strategy/impl/BlackFridayDiscountService.java
+++ src/main/java/core/basesyntax/strategy/impl/BlackFridayDiscountService.java
@@ -3,10 +3,8 @@ package core.basesyntax.strategy.impl;
 import core.basesyntax.strategy.DiscountService;
 
 public class BlackFridayDiscountService implements DiscountService {
-    private static final int DISCOUNT = 45;
-
     @Override
     public double getDiscount() {
-        return DISCOUNT;
+        return 45;
     }
 }
--- src/main/java/core/basesyntax/strategy/impl/DefaultDiscountService.java
+++ src/main/java/core/basesyntax/strategy/impl/DefaultDiscountService.java
@@ -3,10 +3,8 @@ package core.basesyntax.strategy.impl;
 import core.basesyntax.strategy.DiscountService;
 
 public class DefaultDiscountService implements DiscountService {
-    private static final int DISCOUNT = 0;
-
     @Override
     public double getDiscount() {
-        return DISCOUNT;
+        return 0;
     }
 }
