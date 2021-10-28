6109f97fff93cd21da51fa1a0412d4d2cd9c6018
Illia Lavryniuk <lavryniuk.dev@gmail.com>
2021-10-28 19:35:28 +0300

    discount constans were added

--- src/main/java/core/basesyntax/DiscountStrategy.java
+++ src/main/java/core/basesyntax/DiscountStrategy.java
@@ -7,13 +7,17 @@ import core.basesyntax.strategy.impl.DefaultDiscountService;
 import core.basesyntax.strategy.impl.NewYearDiscountService;
 
 public class DiscountStrategy {
+    private static final String BIRTHDAY_DISCOUNT = "Birthday";
+    private static final String BLACK_FRIDAY_DISCOUNT = "Black Friday";
+    private static final String NEW_YEAR_DISCOUNT = "New Year";
+
     public DiscountService getDiscountServiceBySpecialEvent(String specialEvent) {
         switch (specialEvent) {
-            case "Birthday" :
+            case BIRTHDAY_DISCOUNT :
                 return new BirthdayDiscountService();
-            case "Black Friday" :
+            case BLACK_FRIDAY_DISCOUNT :
                 return new BlackFridayDiscountService();
-            case "New Year" :
+            case NEW_YEAR_DISCOUNT :
                 return new NewYearDiscountService();
             default :
                 return new DefaultDiscountService();
