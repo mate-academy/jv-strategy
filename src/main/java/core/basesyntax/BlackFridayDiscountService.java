26863a3f192cc8d78a37fd78316b79c771e3ceb0
Eugene-exe <zhekacauselove@gmail.com>
2021-10-28 21:25:52 +0300

    My solution

---
+++ src/main/java/core/basesyntax/BlackFridayDiscountService.java
@@ -0,0 +1,10 @@
+package core.basesyntax;
+
+import core.basesyntax.strategy.DiscountService;
+
+public class BlackFridayDiscountService implements DiscountService {
+    @Override
+    public double getDiscount() {
+        return 45d;
+    }
+}
---
+++ src/main/java/core/basesyntax/NewYearDiscountService.java
@@ -0,0 +1,10 @@
+package core.basesyntax;
+
+import core.basesyntax.strategy.DiscountService;
+
+public class NewYearDiscountService implements DiscountService {
+    @Override
+    public double getDiscount() {
+        return 20d;
+    }
+}
