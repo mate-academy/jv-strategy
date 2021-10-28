17c1ae170e98c0c78d13ee5b8f28687e7f81d85b
Oleh Mazurkov <o.mazurkov@gmail.com>
2021-10-28 17:50:26 +0300

    solving hw-5 discount v3

--- src/main/java/core/basesyntax/strategy/impl/NewYearDiscountService.java
+++ src/main/java/core/basesyntax/strategy/impl/NewYearDiscountService.java
@@ -1,6 +1,5 @@
 package core.basesyntax.strategy.impl;
 
-import core.basesyntax.DiscountStrategy;
 import core.basesyntax.strategy.DiscountService;
 
 public class NewYearDiscountService implements DiscountService {
