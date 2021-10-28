6255c4c16800f2523d1d9a963d41d241f2e34f19
Artik <Artik25942@gmail.com>
2021-10-28 18:50:14 +0300

    Stretegy work, need mvn clean

---
+++ src/main/java/core/basesyntax/BirthdayDiscountService.java
@@ -0,0 +1,12 @@
+package core.basesyntax;
+
+import core.basesyntax.strategy.DiscountService;
+
+public class BirthdayDiscountService implements DiscountService {
+    @Override
+    public double getDiscount() {
+        return 33;
+    }
+
+
+}
