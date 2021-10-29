c9a228652c5338903708d5bce48f58dba41b30f7
Yurii Khomiak <ykhomiak@st.swps.edu.pl>
2021-10-29 14:36:30 +0200

    Redundant constants were deleted

--- src/main/java/core/basesyntax/strategy/impl/BirthdayDiscountService.java
+++ src/main/java/core/basesyntax/strategy/impl/BirthdayDiscountService.java
@@ -3,10 +3,8 @@ package core.basesyntax.strategy.impl;
 import core.basesyntax.strategy.DiscountService;
 
 public class BirthdayDiscountService implements DiscountService {
-    private static final int BIRTHDAY_DISCOUNT = 33;
-
     @Override
     public double getDiscount() {
-        return BIRTHDAY_DISCOUNT;
+        return 33;
     }
 }
--- src/main/java/core/basesyntax/strategy/impl/BlackFridayDiscountService.java
+++ src/main/java/core/basesyntax/strategy/impl/BlackFridayDiscountService.java
@@ -3,10 +3,8 @@ package core.basesyntax.strategy.impl;
 import core.basesyntax.strategy.DiscountService;
 
 public class BlackFridayDiscountService implements DiscountService {
-    private static final int BLACK_FRIDAY_DISCOUNT = 45;
-
     @Override
     public double getDiscount() {
-        return BLACK_FRIDAY_DISCOUNT;
+        return 45;
     }
 }
