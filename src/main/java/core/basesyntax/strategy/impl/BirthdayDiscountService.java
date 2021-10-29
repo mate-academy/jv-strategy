b476380e67836851e5a5af873fea044ce320d1c3
Yurii Khomiak <ykhomiak@st.swps.edu.pl>
2021-10-29 13:09:05 +0200

    Special events discount strategy was implemented

---
+++ src/main/java/core/basesyntax/strategy/impl/BirthdayDiscountService.java
@@ -0,0 +1,12 @@
+package core.basesyntax.strategy.impl;
+
+import core.basesyntax.strategy.DiscountService;
+
+public class BirthdayDiscountService implements DiscountService {
+    private static final int BIRTHDAY_DISCOUNT = 33;
+
+    @Override
+    public double getDiscount() {
+        return BIRTHDAY_DISCOUNT;
+    }
+}
