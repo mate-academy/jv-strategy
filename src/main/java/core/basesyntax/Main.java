e6a5b83702816f29cc24914823fadd8462f8f6f9
Serhii Pedchenko <djmolot@gmail.com>
2021-11-03 13:06:26 +0200

    tested DiscountStrategy in Main() method

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,4 +1,15 @@
 package core.basesyntax;
 
+import core.basesyntax.strategy.DiscountService;
+
 public class Main {
+    public static void main(String[] args) {
+        //String ivent = "New Year";
+        String ivent = "jvj,,";
+
+        DiscountStrategy discountStrategy = new DiscountStrategy();
+        DiscountService discountService = discountStrategy.getDiscountServiceBySpecialEvent(ivent);
+        double discount = discountService.getDiscount();
+        System.out.println("Your discount today " + discount + "%");
+    }
 }
