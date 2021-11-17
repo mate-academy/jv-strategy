d07baa11a52e22cfc999d10286d5bd2ae2fa62d2
morozovyrv <72643876+morozovyrv@users.noreply.github.com>
2021-11-17 19:07:31 +0200

    Discount implementation classes moved to impl package

--- src/main/java/core/basesyntax/strategy/BlackFridayDiscountService.java
+++
@@ -1,8 +0,0 @@
-package core.basesyntax.strategy;
-
-public class BlackFridayDiscountService implements DiscountService{
-    @Override
-    public double getDiscount() {
-        return 0;
-    }
-}
--- src/main/java/core/basesyntax/strategy/NewYearDiscountService.java
+++
@@ -1,8 +0,0 @@
-package core.basesyntax.strategy;
-
-public class NewYearDiscountService implements DiscountService{
-    @Override
-    public double getDiscount() {
-        return 0;
-    }
-}
