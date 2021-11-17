f29ee3ea3940fde4ed28e430235db6e5104a2af8
morozovyrv <72643876+morozovyrv@users.noreply.github.com>
2021-11-17 19:20:36 +0200

    impl classes imported

--- src/main/java/core/basesyntax/DiscountStrategy.java
+++ src/main/java/core/basesyntax/DiscountStrategy.java
@@ -1,7 +1,10 @@
 package core.basesyntax;
 
 import core.basesyntax.strategy.DiscountService;
-import core.basesyntax.strategy.impl.*;
+import core.basesyntax.strategy.impl.BirthdayDiscountService;
+import core.basesyntax.strategy.impl.BlackFridayDiscountService;
+import core.basesyntax.strategy.impl.DefaultDiscountService;
+import core.basesyntax.strategy.impl.NewYearDiscountService;
 
 public class DiscountStrategy {
     DiscountService getDiscountServiceBySpecialEvent(String specialEvent) {
