36614871a0ad9e9f7ad8213e591383272af18a7b
Yevhen Fedorchuk <evgenfedorchuk89@gmail.com>
2022-01-10 21:42:39 +0200

    Initial commit

---
+++ src/main/java/core/basesyntax/impl/DefaultDiscountService.java
@@ -0,0 +1,10 @@
+package core.basesyntax.impl;
+
+import core.basesyntax.strategy.DiscountService;
+
+public class DefaultDiscountService implements DiscountService {
+    @Override
+    public double getDiscount(double price) {
+        return price;
+    }
+}
