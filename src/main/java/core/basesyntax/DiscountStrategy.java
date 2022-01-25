68b65fd1b88f47365991280fa1552dbcccda10a8
Andriy_Poluhovic <93582635+pikador10@users.noreply.github.com>
2022-01-25 14:54:10 +0200

    Update DiscountStrategy.java

--- src/main/java/core/basesyntax/DiscountStrategy.java
+++ src/main/java/core/basesyntax/DiscountStrategy.java
@@ -7,17 +7,13 @@ import core.basesyntax.strategy.impl.DefaultDiscountService;
 import core.basesyntax.strategy.impl.NewYearDiscountService;
 
 public class DiscountStrategy {
-    private static final String BIRTHDAY = "Birthday";
-    private static final String BLACK_FRIDAY = "Black Friday";
-    private static final String NEW_YEAR = "New Year";
-
     public DiscountService getDiscountServiceBySpecialEvent(String specialEvent) {
         switch (specialEvent) {
-            case BIRTHDAY:
+            case "Birthday":
                 return new BirthdayDiscountService();
-            case BLACK_FRIDAY:
+            case "Black Friday":
                 return new BlackFridayDiscountService();
-            case NEW_YEAR:
+            case "New Year":
                 return new NewYearDiscountService();
             default:
                 return new DefaultDiscountService();
