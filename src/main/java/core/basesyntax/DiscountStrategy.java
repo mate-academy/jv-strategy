e0f03e55dd6d332bc4a60b20569f65754cf737d9
Liubov Chernohor <liubovchernohor@gmail.com>
2022-01-26 14:21:27 +0200

    classes were updated

--- src/main/java/core/basesyntax/DiscountStrategy.java
+++ src/main/java/core/basesyntax/DiscountStrategy.java
@@ -7,16 +7,16 @@ import core.basesyntax.strategy.impl.DefaultDiscountService;
 import core.basesyntax.strategy.impl.NewYearDiscountService;
 
 public class DiscountStrategy {
-    public static final String BIRTHDAY = "Birthday";
-    public static final String BLACK_FRIDAY = "Black Friday";
-    public static final String NEW_YEAR = "New Year";
-
     public DiscountService getDiscountServiceBySpecialEvent(String specialEvent) {
         switch (specialEvent) {
-            case BIRTHDAY : return new BirthdayDiscountService();
-            case BLACK_FRIDAY : return new BlackFridayDiscountService();
-            case NEW_YEAR: return new NewYearDiscountService();
-            default: return new DefaultDiscountService();
+            case "Birthday":
+                return new BirthdayDiscountService();
+            case "Black Friday":
+                return new BlackFridayDiscountService();
+            case "New Year":
+                return new NewYearDiscountService();
+            default:
+                return new DefaultDiscountService();
         }
     }
 }
--- src/main/java/core/basesyntax/strategy/impl/BirthdayDiscountService.java
+++ src/main/java/core/basesyntax/strategy/impl/BirthdayDiscountService.java
@@ -3,10 +3,8 @@ package core.basesyntax.strategy.impl;
 import core.basesyntax.strategy.DiscountService;
 
 public class BirthdayDiscountService implements DiscountService {
-    private static final int DISCOUNT = 33;
-
     @Override
     public double getDiscount() {
-        return DISCOUNT;
+        return 33;
     }
 }
