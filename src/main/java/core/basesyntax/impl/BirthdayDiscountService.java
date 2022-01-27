82b0a767225d756d25f2b5cac5bc926b075a676b
Oleh Mytiai <mytiai.ov@icloud.com>
2022-01-27 20:08:33 +0200

    deleted method isApplicable from DiscountService, fixed discount services

--- src/main/java/core/basesyntax/impl/BirthdayDiscountService.java
+++ src/main/java/core/basesyntax/impl/BirthdayDiscountService.java
@@ -3,14 +3,8 @@ package core.basesyntax.impl;
 import core.basesyntax.strategy.DiscountService;
 
 public class BirthdayDiscountService implements DiscountService {
-    public static final String BIRTHDAY = "Birthday";
-
     @Override
     public double getDiscount() {
         return 33;
-    }
-
-    public boolean isApplicable(String specialEvent) {
-        return BIRTHDAY.equals(specialEvent);
     }
 }
--- src/main/java/core/basesyntax/impl/NewYearDiscountService.java
+++ src/main/java/core/basesyntax/impl/NewYearDiscountService.java
@@ -3,14 +3,8 @@ package core.basesyntax.impl;
 import core.basesyntax.strategy.DiscountService;
 
 public class NewYearDiscountService implements DiscountService {
-    public static final String NEW_YEAR = "New Year";
-
     @Override
     public double getDiscount() {
         return 20;
-    }
-
-    public boolean isApplicable(String specialEvent) {
-        return NEW_YEAR.equals(specialEvent);
     }
 }
