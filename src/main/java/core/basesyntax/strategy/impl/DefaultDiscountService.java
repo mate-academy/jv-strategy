e398841f19ab70bf1372882fcbd1d9e4d19a39d2
Alina <vasylyshyn.alina@gmail.com>
2022-03-23 20:17:50 +0200

    checkstyle

--- src/main/java/core/basesyntax/strategy/impl/DefaultDiscountService.java
+++ src/main/java/core/basesyntax/strategy/impl/DefaultDiscountService.java
@@ -1,7 +1,6 @@
 package core.basesyntax.strategy.impl;
 
 public class DefaultDiscountService implements core.basesyntax.strategy.DiscountService {
-
     @Override
     public double getDiscount() {
         return 0;
