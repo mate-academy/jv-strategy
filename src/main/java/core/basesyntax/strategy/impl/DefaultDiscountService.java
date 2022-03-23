558a1958a8679fbf7a392cdc20dfe530a04ad820
Alina <vasylyshyn.alina@gmail.com>
2022-03-23 20:16:04 +0200

    added implementations for discount

---
+++ src/main/java/core/basesyntax/strategy/impl/DefaultDiscountService.java
@@ -0,0 +1,9 @@
+package core.basesyntax.strategy.impl;
+
+public class DefaultDiscountService implements core.basesyntax.strategy.DiscountService {
+
+    @Override
+    public double getDiscount() {
+        return 0;
+    }
+}
