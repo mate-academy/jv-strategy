ae19cb490086a874f46837df25a22e0ca6fa68d0
Sergii Turchyn <turchyn13@gmail.com>
2022-03-29 13:38:35 +0300

    Deleted empty lines

--- src/main/java/core/basesyntax/DiscountStrategy.java
+++ src/main/java/core/basesyntax/DiscountStrategy.java
@@ -7,7 +7,6 @@ import core.basesyntax.strategy.impl.DefaultDiscountService;
 import core.basesyntax.strategy.impl.NewYearDiscountService;
 
 public class DiscountStrategy {
-
     public DiscountService getDiscountServiceBySpecialEvent(String specialEvent) {
         switch (specialEvent) {
             case "New Year":
@@ -21,4 +20,3 @@ public class DiscountStrategy {
         }
     }
 }
-
--- src/main/java/core/basesyntax/strategy/DiscountService.java
+++ src/main/java/core/basesyntax/strategy/DiscountService.java
@@ -1,6 +1,5 @@
 package core.basesyntax.strategy;
 
 public interface DiscountService {
-
     public double getDiscount();
 }
