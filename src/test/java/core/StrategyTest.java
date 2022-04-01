3be53a79b2dbf3f917ae9550e7c285711c61a887
Dmytro Skrypnyk <skripnik139@gmail.com>
2022-04-01 08:59:28 +0200

    fix mistake in test

--- src/test/java/core/StrategyTest.java
+++ src/test/java/core/StrategyTest.java
@@ -64,7 +64,7 @@ public class StrategyTest {
       Class<?> discountStrategyClass = Class.forName("core.basesyntax.DiscountService");
       checkHasMethodGetDiscountServiceBySpecialEvent(discountStrategyClass);
     } catch (ClassNotFoundException e) {
-      Assert.assertTrue("Class DiscountService isn't created " +
+      Assert.assertTrue("Class DiscountStrategy isn't created " +
           "or created in wrong directory\n", true);
     }
   }
