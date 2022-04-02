188dd580b7d8616d162c9850ca5c412454bde663
Gigawatt94 <101740223+Gigawatt94@users.noreply.github.com>
2022-04-02 21:25:50 +0300

    mvn check: build success

--- src/main/java/core/basesyntax/DiscountStrategy.java
+++ src/main/java/core/basesyntax/DiscountStrategy.java
@@ -1,4 +1,5 @@
 package core.basesyntax;
+
 import core.basesyntax.strategy.DiscountService;
 import core.basesyntax.strategy.impl.Birthday;
 import core.basesyntax.strategy.impl.BlackFriday;
@@ -6,14 +7,12 @@ import core.basesyntax.strategy.impl.DefaultDiscountService;
 import core.basesyntax.strategy.impl.NewYear;
 
 public class DiscountStrategy {
-     DiscountService getDiscountServiceBySpecialEvent(String specialEvent) {
+    DiscountService getDiscountServiceBySpecialEvent(String specialEvent) {
         switch (specialEvent) {
             case ("Birthday") : return new Birthday();
             case ("Black Friday") : return new BlackFriday();
-            case ("New Year") : return  new NewYear();
+            case ("New Year") : return new NewYear();
             default: return new DefaultDiscountService();
         }
-
-
-     }
+    }
 }
