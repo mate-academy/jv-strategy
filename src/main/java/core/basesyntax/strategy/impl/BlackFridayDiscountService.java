86785e167f212b76fd9b3e70ad19d38aef5ff534
Mark Shapka <mark.shapka1@gmail.com>
2022-04-03 14:50:23 +0300

    - changed return value in four classes of *DiscountService

--- src/main/java/core/basesyntax/strategy/impl/BlackFridayDiscountService.java
+++ src/main/java/core/basesyntax/strategy/impl/BlackFridayDiscountService.java
@@ -5,6 +5,6 @@ import core.basesyntax.strategy.DiscountService;
 public class BlackFridayDiscountService implements DiscountService {
     @Override
     public double getDiscount() {
-        return (double) 45;
+        return 45;
     }
 }
