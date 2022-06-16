3035e96aa2d9059e96d85bfe4e4e8b7f94965b0a
Veronika-Alekseienko <alekseienko.n@gmail.com>
2022-06-16 14:14:58 +0200

    "the best solution"

---
+++ src/main/java/core/basesyntax/strategy/impl/NewYearDiscountService.java
@@ -0,0 +1,11 @@
+package core.basesyntax.strategy.impl;
+
+import core.basesyntax.strategy.DiscountService;
+
+public class NewYearDiscountService implements DiscountService {
+    private static final double NEW_YEAR_DISCOUNT = 20;
+    @Override
+    public double getDiscount() {
+        return NEW_YEAR_DISCOUNT;
+    }
+}
