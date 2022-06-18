f24e9d711295c9ec290da52fbe50fa2346ec831d
Nata <natserbo@gmail.com>
2022-06-18 17:42:42 +0300

    mvn corrections

--- src/main/java/core/basesyntax/DiscountStrategy.java
+++ src/main/java/core/basesyntax/DiscountStrategy.java
@@ -7,8 +7,8 @@ import core.basesyntax.strategy.impl.DefaultDiscountService;
 import core.basesyntax.strategy.impl.NewYearDiscountService;
 
 public class DiscountStrategy {
-    public DiscountService getDiscountServiceBySpecialEvent(String specialEvent){
-        switch (specialEvent){
+    public DiscountService getDiscountServiceBySpecialEvent(String specialEvent) {
+        switch (specialEvent) {
             case "Birthday":
                 return new BirthdayDiscountService();
             case "New Year":
