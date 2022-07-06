87308eec9902d0f7aacf6ef0186c4ca1ca078b20
Serhii Buria <sergeiburya@gmail.com>
2022-07-06 12:01:50 +0300

    added interface, template and classes to implement the taskÑ

--- src/main/java/core/basesyntax/MainDiscountService.java
+++ src/main/java/core/basesyntax/MainDiscountService.java
@@ -1,9 +1,15 @@
 package core.basesyntax;
 
+import core.basesyntax.strategy.DiscountService;
+
 public class MainDiscountService {
     public static void main(String[] args) {
 
+        DiscountStrategy discountStrategy = new DiscountStrategy();
+        DiscountService discountService = discountStrategy
+                .getDiscountServiceBySpecialEvent("Black Friday");
+        double discountPrice = discountService.getDiscount();
+        System.out.println(discountPrice);
 
     }
-
 }
--- src/main/java/core/basesyntax/strategy/DiscountService.java
+++ src/main/java/core/basesyntax/strategy/DiscountService.java
@@ -1,8 +1,5 @@
 package core.basesyntax.strategy;
 
 public interface DiscountService {
-
-    public default double getDiscount(){
-        return 0;
-    }
+    public double getDiscount();
 }
