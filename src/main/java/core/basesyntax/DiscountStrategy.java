f56982c4ccc3800a4ca34660a2a9d290bd1bd015
Mikhail Maiorov <majorov.michail@gmail.com>
2022-07-27 19:37:56 +0300

    class DiscountStrategy was created -method getDiscountServiceBySpecialEvent() was implemented

---
+++ src/main/java/core/basesyntax/DiscountStrategy.java
@@ -0,0 +1,21 @@
+package core.basesyntax;
+
+import core.basesyntax.strategy.DiscountService;
+import core.basesyntax.strategy.impl.BirthdayDiscountService;
+import core.basesyntax.strategy.impl.BlackFridayDiscountService;
+import core.basesyntax.strategy.impl.DefaultDiscountService;
+import core.basesyntax.strategy.impl.NewYearDiscountService;
+
+public class DiscountStrategy {
+    public DiscountService getDiscountServiceBySpecialEvent(String specialEvent) {
+        switch (specialEvent) {
+            case "New Year"
+                : return new NewYearDiscountService();
+            case "Black Friday"
+                : return new BlackFridayDiscountService();
+            case "Birth Day"
+                : return new BirthdayDiscountService();
+            default: return new DefaultDiscountService();
+        }
+    }
+}
