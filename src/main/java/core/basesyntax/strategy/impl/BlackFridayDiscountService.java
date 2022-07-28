8aaec65fc8cbb03d79a7eb278344d3f2e0369aeb
Oleksandr-Tovstyi <raketaplaneta@gmail.com>
2022-07-28 08:47:00 +0300

    interface DiscountService and its implementations were created

---
+++ src/main/java/core/basesyntax/strategy/impl/BlackFridayDiscountService.java
@@ -0,0 +1,7 @@
+package core.basesyntax.strategy.impl;
+
+public class BlackFridayDiscountService implements DiscountService {
+    public double getDiscount() {
+        return 45;
+    };
+}
---
+++ src/main/java/core/basesyntax/strategy/impl/NewYearDiscountService.java
@@ -0,0 +1,7 @@
+package core.basesyntax.strategy.impl;
+
+public class NewYearDiscountService implements DiscountService {
+    public double getDiscount()  {
+        return 20;
+    }
+}
