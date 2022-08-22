2f5314849f6600100135e71708410ab4f5b6fbcc
Kostiantyn Sacheiev <dynamofan1927@gmail.com>
2022-08-22 12:26:39 +0200

    Solution was added: 1. Add interface DiscountService 2. Add method getDiscount() to below interface; 3. Add classes DefaultDiscountService                NewYearDiscountService                BirthdayDiscountService                BlackFridayDiscountService Which implements DiscountService and Override getDiscount(); 4. Add class DiscountStrategy which realized method getDiscountServiceBySpecialEvent(String specialEvent)

---
+++ src/main/java/core/basesyntax/straregy/impl/BlackFridayDiscountService.java
@@ -0,0 +1,10 @@
+package core.basesyntax.straregy.impl;
+
+import core.basesyntax.straregy.DiscountService;
+
+public class BlackFridayDiscountService implements DiscountService {
+    @Override
+    public double getDiscount() {
+        return 45;
+    }
+}
