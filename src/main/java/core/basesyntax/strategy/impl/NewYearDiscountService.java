469b3753cb060143df94c669954add5bfc77649c
Anastasia Vasylyk <anastasiavasylykv@gmail.com>
2022-10-04 18:11:21 +0300

    Discount Servise was realized

--- src/main/java/core/basesyntax/strategy/impl/NewYearDiscountService.java
+++ src/main/java/core/basesyntax/strategy/impl/NewYearDiscountService.java
@@ -3,10 +3,8 @@ package core.basesyntax.strategy.impl;
 import core.basesyntax.strategy.DiscountService;
 
 public class NewYearDiscountService implements DiscountService {
-    private static final double DISCOUNT = 20;
-
     @Override
     public double getDiscount() {
-        return DISCOUNT;
+        return 20;
     }
 }
