d8e022d62b405298f4b63b53ecdd2431e13354f0
Oleksiy Masaltsev <a.o.masaltsev@gmail.com>
2022-10-05 17:49:19 +0300

    some literals replaced with constants

--- src/main/java/core/basesyntax/DiscountStrategy.java
+++ src/main/java/core/basesyntax/DiscountStrategy.java
@@ -7,13 +7,17 @@ import core.basesyntax.strategy.impl.DefaultDiscountService;
 import core.basesyntax.strategy.impl.NewYearDiscountService;
 
 public class DiscountStrategy {
+    public static final String BIRTHDAY = "Birthday";
+    public static final String BLACK_FRIDAY = "Black Friday";
+    public static final String NEW_YEAR = "New Year";
+
     DiscountService getDiscountServiceBySpecialEvent(String specialEvent) {
         switch (specialEvent) {
-            case "Birthday":
+            case BIRTHDAY:
                 return new BirthdayDiscountService();
-            case "Black Friday":
+            case BLACK_FRIDAY:
                 return new BlackFridayDiscountService();
-            case "New Year":
+            case NEW_YEAR:
                 return new NewYearDiscountService();
             default: return new DefaultDiscountService();
         }
