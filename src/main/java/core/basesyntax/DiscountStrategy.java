5e7125e827141ad6a0cd3d1ec06ce46e81f48448
Olexii Melnik <alexmelnikk@gmail.com>
2022-10-11 10:30:00 +0300

    DiscountService creation.

---
+++ src/main/java/core/basesyntax/DiscountStrategy.java
@@ -0,0 +1,17 @@
+package core.basesyntax;
+
+public class DiscountStrategy {
+
+    public DiscountService getDiscountServiceBySpecialEvent(String specialEvent) {
+        switch (specialEvent) {
+            case "Birthday" :
+                return new BirthdayDiscountService();
+            case "Black Friday" :
+                return new BlackFridayDiscountService();
+            case "New Year" :
+                return new NewYearDiscountService();
+            default:
+                return new DefaultDiscountService();
+        }
+    }
+}
