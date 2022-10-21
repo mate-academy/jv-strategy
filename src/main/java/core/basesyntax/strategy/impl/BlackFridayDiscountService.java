d41968c8e35a33ab3894c0326e5d00bd832858ce
Victoria Kharlanchuk <kaplabetty@gmail.com>
2022-10-21 17:50:52 +0200

    solution discount strategy

---
+++ src/main/java/core/basesyntax/strategy/impl/BlackFridayDiscountService.java
@@ -0,0 +1,12 @@
+package core.basesyntax.strategy.impl;
+
+import core.basesyntax.strategy.DiscountService;
+
+public class BlackFridayDiscountService implements DiscountService {
+
+    @Override
+    public double getDiscount() {
+        double discount = 45.0;
+        return discount;
+    }
+}
