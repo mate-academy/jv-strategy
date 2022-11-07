9d5e024f6e67b0e1d9dc4e92849e465547cb53b9
Kateryna Takhtai <ktakhtay22@gmail.com>
2022-11-07 15:39:40 +0100

    attempt 2: override method

--- src/main/java/core/basesyntax/DiscountStrategy.java
+++ src/main/java/core/basesyntax/DiscountStrategy.java
@@ -1,18 +1,23 @@
 package core.basesyntax;
 
+import core.basesyntax.strategy.DiscountService;
 import core.basesyntax.strategy.impl.BirthdayDiscountService;
 import core.basesyntax.strategy.impl.BlackFridayDiscountService;
 import core.basesyntax.strategy.impl.DefaultDiscountService;
-import core.basesyntax.strategy.impl.NewYearDiscountService;
 
 public class DiscountStrategy {
-    public NewYearDiscountService getDiscountServiceBySpecialEvent(String specialEvent) {
+    public DiscountService getDiscountServiceBySpecialEvent(String specialEvent) {
         switch(specialEvent) {
             case("New Year"):
-                return new NewYearDiscountService();
+                return new DiscountService() {
+                    @Override
+                    public double getDiscount() {
+                        return 20.0;
+                    }
+                };
             case ("Birthday"):
                 return new BirthdayDiscountService();
-            case("BlackFriday"):
+            case("Black Friday"):
                 return new BlackFridayDiscountService();
             case("none"):
             default:
--- src/main/java/core/basesyntax/strategy/impl/NewYearDiscountService.java
+++ src/main/java/core/basesyntax/strategy/impl/NewYearDiscountService.java
@@ -1,6 +1,6 @@
 package core.basesyntax.strategy.impl;
 
-import core.basesyntax.DiscountService;
+import core.basesyntax.strategy.DiscountService;
 
 public class NewYearDiscountService implements DiscountService {
     @Override
