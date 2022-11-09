158d6cb93b512d1df632e11b6e1d2a6cf100f06b
Dmytro Koval <dimonkoval@gmail.com>
2022-11-09 09:54:25 +0200

    5

--- src/main/java/core/basesyntax/DiscountService.java
+++ src/main/java/core/basesyntax/DiscountService.java
@@ -2,5 +2,7 @@ package core.basesyntax;
 
 public abstract class DiscountService implements core.basesyntax.strategy.DiscountService {
     @Override
-    public abstract double getDiscount();
+    public double getDiscount(){
+        return  0;
+    }
 }
