9cb585d1506ddfbfb1289aa9197e88351e4b817c
Dmytro Koval <dimonkoval@gmail.com>
2022-11-09 11:44:48 +0200

    7

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -5,7 +5,7 @@ import core.basesyntax.strategy.DiscountService;
 public class Main {
     public static void main(String[] args) {
         DiscountStrategy strategy = new DiscountStrategy();
-        DiscountService discount = strategy.getDiscountServiceBySpecialEvent("New Year");
+        DiscountService discount = strategy.getDiscountServiceBySpecialEvent("Black Friday");
         System.out.println(discount.getDiscount());
     }
 }
