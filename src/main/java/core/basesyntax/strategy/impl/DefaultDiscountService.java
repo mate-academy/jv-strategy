cff205a12613a38e428ea2f77eff00a62bcc3f45
Dmytro Koval <dimonkoval@gmail.com>
2022-11-09 09:45:53 +0200

    4

--- src/main/java/core/basesyntax/strategy/impl/DefaultDiscountService.java
+++ src/main/java/core/basesyntax/strategy/impl/DefaultDiscountService.java
@@ -5,5 +5,5 @@ import core.basesyntax.DiscountService;
 public class DefaultDiscountService extends DiscountService {
 
     @Override
-    public double getDiscount() { return 0; }
+    public double getDiscount() { return 0.0; }
 }
