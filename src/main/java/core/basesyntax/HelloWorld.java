e8685f76b7f1b93dfa65c040232dc6256440ceba
Inna Diachkina <innadyachkina@gmail.com>
2022-12-11 17:06:46 +0200

    implement method strategy

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -1,8 +1,8 @@
 package core.basesyntax;
 
-/**
- * Feel free to remove this class and create your own.
- */
 public class HelloWorld {
-
+    public static void main(String[] args) {
+        DiscountStrategy discountStrategy = new DiscountStrategy();
+        discountStrategy.getDiscountServiceBySpecialEvent("Birthday");
+    }
 }
