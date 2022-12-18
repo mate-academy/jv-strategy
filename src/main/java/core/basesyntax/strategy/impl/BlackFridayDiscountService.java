9467e4c473886577ef58619110e1ed30271f2b70
Solomiia Voloshyn <solomiiavoloshyn@gmail.com>
2022-12-18 16:05:25 +0200

    build success achieved

---
+++ src/main/java/core/basesyntax/strategy/impl/BlackFridayDiscountService.java
@@ -0,0 +1,11 @@
+package core.basesyntax.strategy.impl;
+
+import core.basesyntax.DiscountStrategy;
+import core.basesyntax.strategy.DiscountService;
+
+public class BlackFridayDiscountService extends DiscountStrategy implements DiscountService {
+    @Override
+    public double getDiscount() {
+        return 45;
+    }
+}
---
+++ src/main/java/core/basesyntax/strategy/impl/NewYearDiscountService.java
@@ -0,0 +1,11 @@
+package core.basesyntax.strategy.impl;
+
+import core.basesyntax.DiscountStrategy;
+import core.basesyntax.strategy.DiscountService;
+
+public class NewYearDiscountService extends DiscountStrategy implements DiscountService {
+    @Override
+    public double getDiscount() {
+        return 20;
+    }
+}
