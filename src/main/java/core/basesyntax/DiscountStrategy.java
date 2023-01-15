bdd91cb3e92fd3a608a3726db76aa66ef737f650
Kostia Vershynin <vershynin.kostia@gmail.com>
2023-01-15 21:13:38 +0200

    realisation Strategy pattern

--- src/main/java/core/basesyntax/DiscountStrategy.java
+++ src/main/java/core/basesyntax/DiscountStrategy.java
@@ -7,9 +7,9 @@ import core.basesyntax.strategy.impl.DefaultDiscountService;
 import core.basesyntax.strategy.impl.NewYearDiscountService;
 
 public class DiscountStrategy {
-    private final static String BIRTHDAY_EVENT = "Birthday";
-    private final static String BLACK_FRIDAY_EVENT = "Black Friday";
-    private final static String NEW_YEAR_EVENT = "New Year";
+    private static final String BIRTHDAY_EVENT = "Birthday";
+    private static final String BLACK_FRIDAY_EVENT = "Black Friday";
+    private static final String NEW_YEAR_EVENT = "New Year";
 
     public DiscountService getDiscountServiceBySpecialEvent(String specialEvent) {
         switch (specialEvent) {
