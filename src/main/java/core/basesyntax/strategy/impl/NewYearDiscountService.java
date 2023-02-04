da76265d7fdf1665670471d5028858e8a2daed78
Volodymyr Sakalosh <sva2005@ukr.net>
2023-02-04 14:50:23 +0200

    Created class with constant DISCOUNT and override method

---
+++ src/main/java/core/basesyntax/strategy/impl/NewYearDiscountService.java
@@ -0,0 +1,12 @@
+package core.basesyntax.strategy.impl;
+
+import core.basesyntax.strategy.DiscountService;
+
+public class NewYearDiscountService implements DiscountService {
+    private static final double DISCOUNT = 0.2;
+
+    @Override
+    public double getDiscount(double price) {
+        return price * DISCOUNT;
+    }
+}
