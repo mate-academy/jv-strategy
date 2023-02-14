c635b79b1cd0544631884a0c247a092d03e1c0af
Yaroslav Chornyi <123074518+jaroslavcyk@users.noreply.github.com>
2023-02-15 00:03:20 +0200

    Update BlackFridayDiscountService.java

--- src/main/java/core/basesyntax/strategy/impl/BlackFridayDiscountService.java
+++ src/main/java/core/basesyntax/strategy/impl/BlackFridayDiscountService.java
@@ -4,6 +4,7 @@ import core.basesyntax.strategy.DiscountService;
 
 public class BlackFridayDiscountService implements DiscountService {
     private static final double BLACK_FRIDAY_DISCOUNT = 45.0;
+    
     @Override
     public double getDiscount() {
         return BLACK_FRIDAY_DISCOUNT;
