e1a3ceca258ed73e9932ddabf3224c2bb382f28f
Yaroslav Chornyi <123074518+jaroslavcyk@users.noreply.github.com>
2023-02-15 00:06:13 +0200

    Update DiscountStrategy.java

--- src/main/java/core/basesyntax/DiscountStrategy.java
+++ src/main/java/core/basesyntax/DiscountStrategy.java
@@ -6,9 +6,6 @@ import core.basesyntax.strategy.impl.BlackFridayDiscountService;
 import core.basesyntax.strategy.impl.DefaultDiscountService;
 import core.basesyntax.strategy.impl.NewYearDiscountService;
 
-/**
- * Feel free to remove this class and create your own.
- */
 public class DiscountStrategy {
     DiscountService getDiscountServiceBySpecialEvent(String specialEvent) {
         switch (specialEvent) {
