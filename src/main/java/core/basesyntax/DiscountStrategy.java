91bab5c8804b8de6c5a36cf98ad3be69008f17ca
Nikita <nsalogub96@gmail.com>
2023-02-16 14:29:10 +0200

    Implenetation task of discount(2)

--- src/main/java/core/basesyntax/DiscountStrategy.java
+++ src/main/java/core/basesyntax/DiscountStrategy.java
@@ -1,9 +1,9 @@
 package core.basesyntax;
 
-import core.basesyntax.strategy.DiscountService;
 import core.basesyntax.strategy.impl.BirthdayDiscountService;
 import core.basesyntax.strategy.impl.BlackFridayDiscountService;
 import core.basesyntax.strategy.impl.DefaultDiscountService;
+import core.basesyntax.strategy.impl.DiscountService;
 import core.basesyntax.strategy.impl.NewYearDiscountService;
 
 public class DiscountStrategy {
--- src/main/java/core/basesyntax/strategy/impl/BirthdayDiscountService.java
+++ src/main/java/core/basesyntax/strategy/impl/BirthdayDiscountService.java
@@ -1,7 +1,5 @@
 package core.basesyntax.strategy.impl;
 
-import core.basesyntax.strategy.DiscountService;
-
 public class BirthdayDiscountService implements DiscountService {
     @Override
     public double getDiscount() {
