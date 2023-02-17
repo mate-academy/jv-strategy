060452a118f3cb049359848a56c511420a8ede17
Vasyl Sagaidak <saikopixie@gmail.com>
2023-02-17 13:59:45 +0200

    Arranged specialEvent pickup via switch case

--- src/main/java/core/basesyntax/strategy/impl/NewYearDiscountService.java
+++ src/main/java/core/basesyntax/strategy/impl/NewYearDiscountService.java
@@ -1,12 +1,10 @@
 package core.basesyntax.strategy.impl;
 
-import core.basesyntax.DiscountStrategy;
 import core.basesyntax.strategy.DiscountService;
 
-public class NewYearDiscountService extends DiscountStrategy
-        implements DiscountService {
+public class NewYearDiscountService implements DiscountService {
     @Override
     public double getDiscount() {
-        return 0.0;
+        return 20.0;
     }
 }
