24c58debd5fa3841f4aaa5664ebfbb26c0317d25
Oles <a.vykhovanok@gmail.com>
2023-03-29 17:37:27 +0300

    checkstyle fix

--- src/main/java/core/basesyntax/DiscountStrategy.java
+++ src/main/java/core/basesyntax/DiscountStrategy.java
@@ -7,15 +7,15 @@ import core.basesyntax.strategy.impl.DefaultDiscountService;
 import core.basesyntax.strategy.impl.NewYearDiscountService;
 
 public class DiscountStrategy {
-    DiscountService getDiscountServiceBySpecialEvent(String specialEvent){
+    public DiscountStrategy() {
+    }
+
+    DiscountService getDiscountServiceBySpecialEvent(String specialEvent) {
         switch (specialEvent) {
             case "Birthday" : return new BirthdayDiscountService();
             case "Black Friday" : return new BlackFridayDiscountService();
             case "New Year" : return new NewYearDiscountService();
             default: return new DefaultDiscountService();
         }
-    }
-
-    public DiscountStrategy() {
     }
 }
