106b5e0b8a2cdf67c78f139f3534e289bb62a69a
Mutaborr <123407682+Mutaborr@users.noreply.github.com>
2023-04-11 20:40:26 +0300

    Delete BlackFridayDiscountService.java

--- src/main/java/core/basesyntax/BlackFridayDiscountService.java
+++
@@ -1,9 +0,0 @@
-package core.basesyntax.strategy.impl;
-
-import core.basesyntax.strategy.DiscountService;
-
-public class BlackFridayDiscountService implements DiscountService {
-    public double getDiscount() {
-        return 45;
-    }
-}
