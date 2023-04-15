4a7569e23251bd8fb9a20bfa087c05c957350518
inbox-reva <inbox.reva@gmail.com>
2023-04-15 10:22:08 +0300

    implement discount system in some special events of a year

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -1,8 +1,16 @@
 package core.basesyntax;
 
+import core.basesyntax.strategy.DiscountService;
+
 /**
  * Feel free to remove this class and create your own.
  */
-public class HelloWorld {
 
+public class HelloWorld {
+    public static void main(String[] args) {
+        DiscountStrategy discountStrategy = new DiscountStrategy();
+        DiscountService disctServ = discountStrategy.getDiscountServiceBySpecialEvent("Birthday");
+        double discount = disctServ.getDiscount();
+        System.out.println(discount);
+    }
 }
