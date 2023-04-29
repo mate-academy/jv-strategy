8a95c1425357427ad0c79b72b98cf5a9723adec5
Pasha <pavlokrasota@gmail.com>
2023-04-29 23:32:30 +0300

    Solved task with using Strategy pattern

---
+++ src/main/java/core/basesyntax/strategy/DiscountStrategy.java
@@ -0,0 +1,16 @@
+package core.basesyntax.strategy;
+
+public class DiscountStrategy {
+    public DiscountService getDiscountServiceBySpecialEvent(String specialEvent) {
+        switch (specialEvent) {
+            case "New Year":
+                return new NewYearDiscountService();
+            case "Birthday":
+                return new BirthdayDiscountService();
+            case "Black Friday":
+                return new BlackFridayDiscountService();
+            default:
+                return new DefaultDiscountService();
+        }
+    }
+}
