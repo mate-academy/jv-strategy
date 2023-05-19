3b8acb45f45175f7c787607e4a10d60fe90c7249
pochtalon <pochtalon1010@gmail.com>
2023-05-19 09:13:36 +0300

    Separated methods by line

--- src/main/java/core/basesyntax/strategy/DiscountStrategy.java
+++ src/main/java/core/basesyntax/strategy/DiscountStrategy.java
@@ -4,6 +4,7 @@ public class DiscountStrategy {
     private static final String BIRTHDAY = "Birthday";
     private static final String BLACK_FRIDAY = "Black Friday";
     private static final String NEW_YEAR = "New Year";
+
     public DiscountService getDiscountServiceBySpecialEvent(String specialEvent) {
         switch (specialEvent) {
             case (NEW_YEAR): return new NewYearDiscountService();
