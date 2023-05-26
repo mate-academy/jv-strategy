275aa9453901bfd150ce86e4f686c89e25e50ff8
Mykhailo <mykhailokuryk16@gmail.com>
2023-05-26 20:34:09 +0300

    add solution

---
+++ src/main/java/core/basesyntax/DiscountStrategy.java
@@ -0,0 +1,16 @@
+package core.basesyntax;
+
+public class DiscountStrategy {
+    DiscountService getDiscountServiceBySpecialEvent(String specialEvent){
+        switch (specialEvent) {
+            case "Birthday":
+                return new BirthdayDiscountService();
+            case "Black Friday":
+                return new BlackFridayDiscountService();
+            case "New Year":
+                return new NewYearDiscountService();
+            default:
+                return new DefaultDiscountService();
+        }
+    }
+}
