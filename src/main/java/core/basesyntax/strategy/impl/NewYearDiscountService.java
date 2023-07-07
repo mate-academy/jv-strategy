d36630cb8eb8553a28c88d4eaec3722ef0ef9da2
Ihor <kalaurdelta96@gmail.com>
2023-07-07 20:14:46 +0300

    added solution

--- src/main/java/core/basesyntax/strategy/impl/NewYearDiscountService.java
+++ src/main/java/core/basesyntax/strategy/impl/NewYearDiscountService.java
@@ -5,6 +5,6 @@ import core.basesyntax.strategy.DiscountService;
 public class NewYearDiscountService implements DiscountService {
     @Override
     public double getDiscount() {
-        return 33;
+        return 20;
     }
 }
