1005dd9cd716a80168a3f798c85407acb7f37b4d
literandltx <romanskovronn@gmail.com>
2023-07-16 15:10:49 +0300

    add main logic

--- src/main/java/core/basesyntax/DiscountStrategy.java
+++ src/main/java/core/basesyntax/DiscountStrategy.java
@@ -4,6 +4,17 @@ import core.basesyntax.strategy.DiscountService;
 
 public class DiscountStrategy {
     public DiscountService getDiscountServiceBySpecialEvent(String specialEvent) {
-        return () -> 0;
+        switch (specialEvent) {
+            case "Default event":
+                return () -> 0;
+            case "Birthday":
+                return () -> 33;
+            case "Black Friday":
+                return () -> 45;
+            case "New Year":
+                return () -> 20;
+            default:
+                throw new RuntimeException();
+        }
     }
 }
--- src/main/java/core/basesyntax/strategy/impl/NewYearDiscountService.java
+++ src/main/java/core/basesyntax/strategy/impl/NewYearDiscountService.java
@@ -1,4 +1,10 @@
 package core.basesyntax.strategy.impl;
 
-public class NewYearDiscountService {
+import core.basesyntax.strategy.DiscountService;
+
+public class NewYearDiscountService implements DiscountService {
+    @Override
+    public double getDiscount() {
+        return 20;
+    }
 }
