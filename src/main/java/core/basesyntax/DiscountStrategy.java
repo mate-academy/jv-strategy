ec8562696c23050fed362a7160f4bace1f3dfa4e
Kirkir777 <106085319+Kirkir777@users.noreply.github.com>
2023-07-24 16:40:21 +0300

    added changes to this class

---
+++ src/main/java/core/basesyntax/DiscountStrategy.java
@@ -0,0 +1,19 @@
+package core.basesyntax;
+
+import core.basesyntax.strategy.DiscountService;
+import core.basesyntax.strategy.impl.*;
+
+public class DiscountStrategy {
+    public DiscountService getDiscountServiceBySpecialEvent(String specialEvent) {
+        switch (specialEvent) {
+            case "New Year":
+                return new NewYearDiscountService();
+            case "Birthday":
+                return new BirthdayDiscountService();
+            case "Black Friday":
+                return new BlackFridayDiscountService();
+            default:
+                return new DefaultDiscountService();
+        }
+    }
+}
