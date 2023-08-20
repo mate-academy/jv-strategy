5d4b8a5c50d9934f14c2a28bef367be984e8da4c
Volodymyr Hryhoryk <volodymyrhryhoryk@gmail.com>
2023-08-20 19:09:04 +0300

    added fixes

--- src/main/java/core/basesyntax/strategy/impl/BlackFridayDiscountService.java
+++ src/main/java/core/basesyntax/strategy/impl/BlackFridayDiscountService.java
@@ -3,6 +3,7 @@ package core.basesyntax.strategy.impl;
 import core.basesyntax.strategy.DiscountService;
 
 public class BlackFridayDiscountService implements DiscountService {
+
     @Override
     public double getDiscount() {
         return 45.0;
--- src/main/java/core/basesyntax/strategy/impl/NewYearDiscountService.java
+++ src/main/java/core/basesyntax/strategy/impl/NewYearDiscountService.java
@@ -3,6 +3,7 @@ package core.basesyntax.strategy.impl;
 import core.basesyntax.strategy.DiscountService;
 
 public class NewYearDiscountService implements DiscountService {
+
     @Override
     public double getDiscount() {
         return 20.0;
