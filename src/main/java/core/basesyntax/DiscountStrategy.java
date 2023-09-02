cab6fe6ceb47fb439c95123f304a06954cc315c0
Vlad Rublenko <vladik.rar@ukr.net>
2023-09-02 17:38:37 +0300

    initial commit

---
+++ src/main/java/core/basesyntax/DiscountStrategy.java
@@ -0,0 +1,24 @@
+package core.basesyntax;
+
+import core.basesyntax.impl.BlackFridayDiscountService;
+import core.basesyntax.impl.BirthdayDiscountService;
+import core.basesyntax.impl.DefaultDiscountService;
+import core.basesyntax.impl.NewYearDiscountService;
+
+import core.basesyntax.strategy.DiscountService;
+
+public class DiscountStrategy {
+
+    public DiscountService getDiscountServiceBySpecialEvent(String speacialEvent) {
+        switch (speacialEvent) {
+            case "Birthday":
+                return new BirthdayDiscountService();
+            case "Black Friday":
+                return  new BlackFridayDiscountService();
+            case "New Year":
+                return new NewYearDiscountService();
+            default:
+                return new DefaultDiscountService();
+        }
+    }
+}
