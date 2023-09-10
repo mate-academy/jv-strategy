f102f565510f1ba43060b472f41643067d571569
Lukian Muzyka <lukianmuzyka@gmail.com>
2023-09-10 20:45:25 +0300

    added realisation

---
+++ src/main/java/core/basesyntax/Application.java
@@ -0,0 +1,20 @@
+package core.basesyntax;
+
+import core.basesyntax.strategy.DiscountService;
+
+public class Application {
+    private static final DiscountStrategy STRATEGY = new DiscountStrategy();
+    private static double DISCOUNT;
+    private static final String SPECIAL_EVENT = "New Year";
+
+    public static void main(String[] args) {
+        DiscountService blackFridayService = STRATEGY.getDiscountServiceBySpecialEvent(SPECIAL_EVENT);
+        DISCOUNT = blackFridayService.getDiscount();
+        System.out.println("Congratulations! You have "
+                + DISCOUNT
+                + "%"
+                + " off with our "
+                + SPECIAL_EVENT
+                + " discount!");
+    }
+}
