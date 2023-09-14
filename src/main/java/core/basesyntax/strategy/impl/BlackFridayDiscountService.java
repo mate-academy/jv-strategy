5658169b1078eab160dc04c3da95b20abcdecd7f
Roman Menzheres <roma.menzheres@gmail.com>
2023-09-14 14:34:50 +0300

    Converted all magic number into const

--- src/main/java/core/basesyntax/strategy/impl/BlackFridayDiscountService.java
+++ src/main/java/core/basesyntax/strategy/impl/BlackFridayDiscountService.java
@@ -3,9 +3,10 @@ package core.basesyntax.strategy.impl;
 import core.basesyntax.strategy.DiscountService;
 
 public class BlackFridayDiscountService implements DiscountService {
+    private static final int DISCOUNT = 45;
 
     @Override
     public double getDiscount() {
-        return 45;
+        return DISCOUNT;
     }
 }
