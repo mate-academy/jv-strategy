88bb40c5da2cd79dfca707e70d0f0e0343df2047
TymurPanchenk0 <tymur.panchenko@gmail.com>
2023-10-09 22:42:32 +0300

    Strategy functional

--- src/main/java/core/basesyntax/DiscountStrategy.java
+++ src/main/java/core/basesyntax/DiscountStrategy.java
@@ -4,6 +4,7 @@ public class DiscountStrategy {
     public static final String NEW_YEAR = "New Year";
     public static final String BIRTHDAY = "Birthday";
     public static final String BLACK_FRIDAY = "Black Friday";
+
     public DiscountService getDiscountServiceBySpecialEvent(String specialEvent) {
         switch (specialEvent) {
             case NEW_YEAR:
