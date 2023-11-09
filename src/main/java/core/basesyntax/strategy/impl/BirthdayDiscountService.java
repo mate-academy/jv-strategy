f4a95d01bfa83dd46236050d5b7da552cb43d6eb
Bohdan <bohdan.g.54@gmail.com>
2023-11-09 15:50:45 +0200

    fixed birthday discount

--- src/main/java/core/basesyntax/strategy/impl/BirthdayDiscountService.java
+++ src/main/java/core/basesyntax/strategy/impl/BirthdayDiscountService.java
@@ -5,6 +5,6 @@ import core.basesyntax.strategy.DiscountService;
 public class BirthdayDiscountService implements DiscountService {
     @Override
     public double getDiscount() {
-        return 35;
+        return 33;
     }
 }
