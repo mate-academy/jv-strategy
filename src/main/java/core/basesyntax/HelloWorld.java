3cfb225d8e557a35fae378e97983b4cafd025eb4
Ivan <lavrinenkoivan51@gmail.com>
2023-11-12 05:02:54 +0100

    strategy pattern used for getting specific discount type

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -1,8 +1,12 @@
 package core.basesyntax;
 
-/**
- * Feel free to remove this class and create your own.
- */
-public class HelloWorld {
+import core.basesyntax.strategy.DiscountService;
 
+public class HelloWorld {
+    public static void main(String [] args) {
+        DiscountStrategy discountStrategy = new DiscountStrategy();
+        DiscountService newYear = discountStrategy.getDiscountServiceBySpecialEvent("New Year");
+        double discount = newYear.getDiscount();
+        System.out.println(discount);
+    }
 }
