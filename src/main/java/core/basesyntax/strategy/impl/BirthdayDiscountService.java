fd3cdee055cf664f10d7c53ead4c17b34f0cdb27
Kinereiko <kinereiko@gmail.com>
2023-12-12 22:47:48 +0200

    Create DiscountService interface, its implementations, DiscountStrategy class

---
+++ src/main/java/core/basesyntax/strategy/impl/BirthdayDiscountService.java
@@ -0,0 +1,12 @@
+package core.basesyntax.strategy.impl;
+
+import core.basesyntax.strategy.DiscountService;
+
+public class BirthdayDiscountService implements DiscountService {
+    private final double discount = 33.0;
+
+    @Override
+    public double getDiscount() {
+        return discount;
+    }
+}
