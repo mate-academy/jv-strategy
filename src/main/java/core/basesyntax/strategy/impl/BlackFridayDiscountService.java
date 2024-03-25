a7261be101f831396ad3ffaa62b8487650320fec
Olexander Savchuk <savchukolexanderwork@gmail.com>
2024-03-25 21:46:23 +0200

    Created different Dicount services with different discount

---
+++ src/main/java/core/basesyntax/strategy/impl/BlackFridayDiscountService.java
@@ -0,0 +1,12 @@
+package core.basesyntax.strategy.impl;
+
+import core.basesyntax.strategy.DiscountService;
+
+public class BlackFridayDiscountService implements DiscountService {
+    private static final double discount = 45;
+
+    @Override
+    public double getDiscount() {
+        return discount;
+    }
+}
