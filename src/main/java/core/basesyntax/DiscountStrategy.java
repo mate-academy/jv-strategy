30dfda6da2161f50087134c942ef1fd15b646d20
SviatoslavTataryn <www.t.svyatoslav55@gmail.com>
2024-04-08 21:32:46 +0200

    create important classes and implementation methods from pattern strategy.

---
+++ src/main/java/core/basesyntax/DiscountStrategy.java
@@ -0,0 +1,19 @@
+package core.basesyntax;
+
+public class DiscountStrategy {
+    public DiscountService getDiscountServiceBySpecialEvent(String specialEvent) {
+        if (specialEvent == null) {
+            return new DefaultDiscountService();
+        }
+        switch (specialEvent) {
+            case "Birthday":
+                return new BirthdayDiscountService();
+            case "Black Friday":
+                return new BlackFridayDiscountService();
+            case "New Year":
+                return new NewYearDiscountService();
+            default:
+                return new DefaultDiscountService();
+        }
+    }
+}
