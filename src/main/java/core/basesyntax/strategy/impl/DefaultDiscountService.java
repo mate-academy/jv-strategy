1833338b31fc61d4ea875abedd5a1e2bfbc2ac8b
Illia Petrov <illiapetrov10@gmail.com>
2024-05-15 19:51:26 +0200

    V 1.0

---
+++ src/main/java/core/basesyntax/strategy/impl/DefaultDiscountService.java
@@ -0,0 +1,11 @@
+package core.basesyntax.strategy.impl;
+
+import core.basesyntax.strategy.DiscountService;
+
+public class DefaultDiscountService implements DiscountService {
+    @Override
+    public double getDiscount() {
+        double discount = 0.0;
+        return discount;
+    }
+}
