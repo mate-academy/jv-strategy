30367f3f41d4c18360785dd2e0d05bb30bdd7f16
Oleksii Deineka <deineka219@gmail.com>
2024-05-21 23:59:10 +0300

    Made up DiscountStrategy according to specialEvent

---
+++ src/main/java/core/basesyntax/DiscountStrategy.java
@@ -0,0 +1,18 @@
+package core.basesyntax;
+
+import core.basesyntax.impl.BirthdayDiscountService;
+import core.basesyntax.impl.BlackFridayDiscountService;
+import core.basesyntax.impl.DefaultDiscountService;
+import core.basesyntax.impl.NewYearDiscountService;
+import core.basesyntax.strategy.DiscountService;
+
+public class DiscountStrategy {
+    public DiscountService getDiscountServiceBySpecialEvent(String specialEvent) {
+        return switch (specialEvent) {
+            case "New Year" -> new NewYearDiscountService();
+            case "Birthday" -> new BirthdayDiscountService();
+            case "Black Friday" -> new BlackFridayDiscountService();
+            default -> new DefaultDiscountService();
+        };
+    }
+}
