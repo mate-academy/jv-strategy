a5f02d1a4116f07fca54c78ee143fe8c7f28b8b4
Alisa Hoys <hoys@protonmail.com>
2024-06-07 14:27:36 +0300

    Finished Strategy

--- src/main/java/core/basesyntax/DiscountStrategy.java
+++ src/main/java/core/basesyntax/DiscountStrategy.java
@@ -5,6 +5,7 @@ import core.basesyntax.strategy.impl.BirthdayDiscountService;
 import core.basesyntax.strategy.impl.BlackFridayDiscountService;
 import core.basesyntax.strategy.impl.DefaultDiscountService;
 import core.basesyntax.strategy.impl.NewYearDiscountService;
+
 public class DiscountStrategy {
     public DiscountService getDiscountServiceBySpecialEvent(String specialEvent) {
         switch (specialEvent) {
