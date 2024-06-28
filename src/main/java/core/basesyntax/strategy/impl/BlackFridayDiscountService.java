edf3ab19fba072596491e9d24db5f61a9e2ef51b
Fedir Lozovyi <sumkin112@gmail.com>
2024-06-28 19:42:01 +0200

    I like to play strategy

--- src/main/java/core/basesyntax/strategy/impl/BlackFridayDiscountService.java
+++ src/main/java/core/basesyntax/strategy/impl/BlackFridayDiscountService.java
@@ -5,6 +5,6 @@ import core.basesyntax.strategy.DiscountService;
 public class BlackFridayDiscountService implements DiscountService {
     @Override
     public double getDiscount() {
-        return  45;
+        return 45;
     }
 }
