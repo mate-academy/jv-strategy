a9c39f961b406bfc3c775e15003066c1b32bdacb
homeworkfirst <zookotelif@gmail.com>
2024-07-19 01:18:37 +0300

    1

---
+++ src/main/java/core/basesyntax/DiscountStrategy.java
@@ -0,0 +1,27 @@
+package core.basesyntax;
+
+import core.basesyntax.strategy.DiscountService;
+import core.basesyntax.strategy.impl.DefaultDiscountService;
+import core.basesyntax.strategy.impl.NewYearDiscountService;
+import core.basesyntax.strategy.impl.BirthdayDiscountService;
+import core.basesyntax.strategy.impl.BlackFridayDiscountService;
+
+public class DiscountStrategy {
+
+    public DiscountService getDiscountServiceBySpecialEvent(String specialEvent) {
+        if (specialEvent == null) {
+            return new DefaultDiscountService();
+        }
+
+        switch (specialEvent) {
+            case "New Year":
+                return new NewYearDiscountService();
+            case "Birthday":
+                return new BirthdayDiscountService();
+            case "Black Friday":
+                return new BlackFridayDiscountService();
+            default:
+                return new DefaultDiscountService();
+        }
+    }
+}
