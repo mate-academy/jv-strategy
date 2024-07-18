f9ca34170a0f441412ce7022f928001b5276344d
homeworkfirst <zookotelif@gmail.com>
2024-07-19 01:23:18 +0300

    1

--- src/main/java/core/basesyntax/DiscountStrategy.java
+++ src/main/java/core/basesyntax/DiscountStrategy.java
@@ -1,10 +1,10 @@
 package core.basesyntax;
 
 import core.basesyntax.strategy.DiscountService;
-import core.basesyntax.strategy.impl.DefaultDiscountService;
-import core.basesyntax.strategy.impl.NewYearDiscountService;
 import core.basesyntax.strategy.impl.BirthdayDiscountService;
 import core.basesyntax.strategy.impl.BlackFridayDiscountService;
+import core.basesyntax.strategy.impl.DefaultDiscountService;
+import core.basesyntax.strategy.impl.NewYearDiscountService;
 
 public class DiscountStrategy {
 
