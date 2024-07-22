0f10bbc3437ec70216ecc1ea1e8911cf18afbc2a
Ivan Koshovyi <ivan2023koshovyi@gmail.com>
2024-07-22 18:23:01 +0300

    implemented 4 DiscountService

--- src/main/java/core/basesyntax/strategy/impl/BirthdayDiscountService.java
+++ src/main/java/core/basesyntax/strategy/impl/BirthdayDiscountService.java
@@ -1,4 +1,10 @@
 package core.basesyntax.strategy.impl;
 
-public class BirthdayDiscountService {
+import core.basesyntax.strategy.DiscountService;
+
+public class BirthdayDiscountService implements DiscountService {
+    @Override
+    public double getDiscount(double price) {
+        return price - (price * 0.45);
+    }
 }
