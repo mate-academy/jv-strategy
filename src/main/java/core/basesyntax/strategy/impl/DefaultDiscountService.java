8df503503789e612848aa30f16b4dc428663e0db
Andrii Riezin <andrey.o.rezin@gmail.com>
2024-08-01 12:28:40 +0300

    implemented DiscountService interface that can calculate discounts.

---
+++ src/main/java/core/basesyntax/strategy/impl/DefaultDiscountService.java
@@ -0,0 +1,12 @@
+package core.basesyntax.strategy.impl;
+
+import core.basesyntax.strategy.DiscountService;
+
+public class DefaultDiscountService implements DiscountService {
+    public static final int DEFAULT_DISCOUNT = 0;
+
+    @Override
+    public double getDiscount() {
+        return DEFAULT_DISCOUNT;
+    }
+}
