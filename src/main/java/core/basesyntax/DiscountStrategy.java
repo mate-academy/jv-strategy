c12c97da71a0018c0c44c5808158eb54281f45c9
Damian Frynas <cz.damax@gmail.com>
2024-08-15 17:33:28 +0200

    Test Pass

--- src/main/java/core/basesyntax/DiscountStrategy.java
+++ src/main/java/core/basesyntax/DiscountStrategy.java
@@ -10,6 +10,11 @@ import core.basesyntax.strategy.impl.NewYearDiscountService;
  * Feel free to remove this class and create your own.
  */
 public class DiscountStrategy {
+    private static void getTest() {
+        System.out.println("test");
+        //This void was added only for my and to let me send pull request cuz iv made a mistake and made this quest on master branch
+    }
+
     DiscountService getDiscountServiceBySpecialEvent(String specialEcent) {
         return switch (specialEcent) {
             case "Birthday" -> new BirthdayDiscountService();
@@ -17,6 +22,7 @@ public class DiscountStrategy {
             case "New Year" -> new NewYearDiscountService();
             default -> new DefaultDiscountService();
         };
+
     }
 }
-//test
+
