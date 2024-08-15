35606fa6a52396051d3f2e57e590ad03b24b1c8c
Damian Frynas <cz.damax@gmail.com>
2024-08-15 17:18:00 +0200

    Created Interface called DiscountService and implements getDiscount() inside. Created implementations for DefaultDiscountService, NewYearDiscountService, BirthdayDiscountService, BlackFridayDiscountService using nessesery discounts in core.basesyntax.strategy.impl. All tests and build passed.

---
+++ src/main/java/core/basesyntax/DiscountStrategy.java
@@ -0,0 +1,21 @@
+package core.basesyntax;
+
+import core.basesyntax.strategy.DiscountService;
+import core.basesyntax.strategy.impl.BirthdayDiscountService;
+import core.basesyntax.strategy.impl.BlackFridayDiscountService;
+import core.basesyntax.strategy.impl.DefaultDiscountService;
+import core.basesyntax.strategy.impl.NewYearDiscountService;
+
+/**
+ * Feel free to remove this class and create your own.
+ */
+public class DiscountStrategy {
+    DiscountService getDiscountServiceBySpecialEvent(String specialEcent) {
+        return switch (specialEcent) {
+            case "Birthday" -> new BirthdayDiscountService();
+            case "Black Friday" -> new BlackFridayDiscountService();
+            case "New Year" -> new NewYearDiscountService();
+            default -> new DefaultDiscountService();
+        };
+    }
+}
