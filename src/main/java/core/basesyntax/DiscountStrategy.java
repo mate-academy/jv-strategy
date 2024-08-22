6c1dba930084fd0fad1f3f5502da3ce2be5573ca
Yehor Nazarets <nazarets1111@gmail.com>
2024-08-22 21:20:32 +0200

    Created interface, classes and methods for calculation Discounts

---
+++ src/main/java/core/basesyntax/DiscountStrategy.java
@@ -0,0 +1,23 @@
+package core.basesyntax;
+
+import core.basesyntax.impl.BirthdayDiscountService;
+import core.basesyntax.impl.BlackFridayDiscountService;
+import core.basesyntax.impl.DefaultDiscountService;
+import core.basesyntax.impl.NewYearDiscountService;
+import core.basesyntax.strategy.DiscountService;
+
+public class DiscountStrategy {
+
+    public DiscountService getDiscountServiceBySpecialEvent(String holiday) {
+        switch (holiday) {
+            case "Birthday":
+                return new BirthdayDiscountService();
+            case "Black Friday":
+                return new BlackFridayDiscountService();
+            case "New Year":
+                return new NewYearDiscountService();
+            default:
+                return new DefaultDiscountService();
+        }
+    }
+}
