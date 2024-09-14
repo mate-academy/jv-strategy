1c16194230b0fc1451712a4b60a2571f855299d1
fwhoami <clahfclns@gmail.com>
2024-09-14 15:24:25 +0300

    Fixing maven build

--- src/main/java/core/basesyntax/DiscountStrategy.java
+++ src/main/java/core/basesyntax/DiscountStrategy.java
@@ -1,6 +1,5 @@
 package core.basesyntax;
 
-
 import core.basesyntax.strategy.DiscountService;
 import core.basesyntax.strategy.impl.BirthdayDiscountService;
 import core.basesyntax.strategy.impl.BlackFridayDiscountService;
