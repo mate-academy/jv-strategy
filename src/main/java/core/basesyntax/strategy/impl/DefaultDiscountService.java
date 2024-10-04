9dd6e81159e6b2e0d9de3bb2779d9d5717900bac
Anastasiia Dihich <an.digich@gmail.com>
2024-10-04 22:29:31 +0300

    hw2

--- src/main/java/core/basesyntax/strategy/impl/DefaultDiscountService.java
+++ src/main/java/core/basesyntax/strategy/impl/DefaultDiscountService.java
@@ -3,10 +3,9 @@ package core.basesyntax.strategy.impl;
 import core.basesyntax.strategy.DiscountService;
 
 public class DefaultDiscountService implements DiscountService {
-    private static final double DISCOUNT_PERCENTAGE = 0.0;
 
     @Override
-    public double getDiscount(double price) {
-        return (price * DISCOUNT_PERCENTAGE) / 100;
+    public double getDiscount() {
+        return 0;
     }
 }
--- src/main/java/core/basesyntax/strategy/impl/NewYearDiscountService.java
+++ src/main/java/core/basesyntax/strategy/impl/NewYearDiscountService.java
@@ -6,7 +6,7 @@ public class NewYearDiscountService implements DiscountService {
     private static final double DISCOUNT_PERCENTAGE = 20.0;
 
     @Override
-    public double getDiscount(double price) {
-        return (price * DISCOUNT_PERCENTAGE) / 100;
+    public double getDiscount() {
+        return DISCOUNT_PERCENTAGE;
     }
 }
