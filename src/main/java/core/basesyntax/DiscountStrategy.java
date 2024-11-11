963827de0660be04cff656e177f981088315425c
Mihail <mishadyakivnich@gmail.com>
2024-11-11 17:34:16 +0200

    correct checkstyle

--- src/main/java/core/basesyntax/DiscountStrategy.java
+++ src/main/java/core/basesyntax/DiscountStrategy.java
@@ -7,16 +7,16 @@ import core.basesyntax.strategy.impl.DefaultDiscountService;
 import core.basesyntax.strategy.impl.NewYearDiscountService;
 
 public class DiscountStrategy {
-     public DiscountService getDiscountServiceBySpecialEvent(String specialEvent) {
-         switch (specialEvent) {
-             case "Birthday":
-                 return new BirthdayDiscountService();
-             case "Black Friday":
-                 return new BlackFridayDiscountService();
-             case "New Year":
-                 return new NewYearDiscountService();
-             default:
-                 return new DefaultDiscountService();
-         }
-     }
+    public DiscountService getDiscountServiceBySpecialEvent(String specialEvent) {
+        switch (specialEvent) {
+            case "Birthday":
+                return new BirthdayDiscountService();
+            case "Black Friday":
+                return new BlackFridayDiscountService();
+            case "New Year":
+                return new NewYearDiscountService();
+            default:
+                return new DefaultDiscountService();
+        }
+    }
 }
