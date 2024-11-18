389c5062fe6ea6fcfafac64f3405be3680370858
Dmytro Borysenko <borisand020774@gmail.com>
2024-11-18 19:24:10 +0200

    https://github.com/DmytroBorysenko2004/jv-strategy

---
+++ src/test/java/core/basesyntax/DefaultDiscountService.java
@@ -0,0 +1,6 @@
+package core.basesyntax;
+public class DefaultDiscountService  implements  DiscountService{
+    public double getDiscount() {
+        return 0.0;
+    }
+}
---
+++ src/test/java/core/basesyntax/NewYearDiscountService.java
@@ -0,0 +1,4 @@
+package core.basesyntax;
+public class NewYearDiscountService implements DiscountService {
+    public double getDiscount() {return 20.0;}
+}
