8de1a5cb050bf75a7e5296512447c49077684605
DmytroBorysenko2004 <borisand020774@gmail.com>
2024-11-18 15:17:21 +0200

    Add files via upload

---
+++ DefaultDiscountService.java
@@ -0,0 +1,7 @@
+package core.basesyntax.strategy.impl;
+import core.basesyntax.strategy.DiscountService;
+public class DefaultDiscountService  implements  DiscountService{
+    public double getDiscount() {
+        return 0.0;
+    }
+}
