f12cf94409be144a400c1d08874a825084bfd7da
DmytroBorysenko2004 <borisand020774@gmail.com>
2024-11-18 16:32:02 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,17 +1,10 @@
+package core.basesyntax;
 import core.basesyntax.strategy.DiscountService;
-import core.basesyntax.strategy.impl.BirthdayDiscountService;
-import core.basesyntax.strategy.impl.DiscountStrategy;
 
 public class Main {
     public static void main(String[] args) {
         DiscountStrategy specialEvent = new DiscountStrategy();
         DiscountService birthdayService = DiscountStrategy.getDiscountServiceBySpecialEvent("Birthday");
         System.out.println(birthdayService.getDiscount() + "%");
-        DiscountService blackFridayService = DiscountStrategy.getDiscountServiceBySpecialEvent("Black Friday");
-        System.out.println(blackFridayService.getDiscount() + "%");
-        DiscountService newYearService = DiscountStrategy.getDiscountServiceBySpecialEvent("New Year");
-        System.out.println(newYearService.getDiscount() + "%");
-        DiscountService defaultService = DiscountStrategy.getDiscountServiceBySpecialEvent("Default");
-        System.out.println(defaultService.getDiscount() + "%");
     }
-    }
+}
--- NewYearDiscountService.java
+++ NewYearDiscountService.java
@@ -1,7 +1,5 @@
 package core.basesyntax.strategy.impl;
 import core.basesyntax.strategy.DiscountService;
 public class NewYearDiscountService implements DiscountService {
-    public double getDiscount() {
-        return 20.0;
-    }
+    public double getDiscount() {return 20.0;}
 }
