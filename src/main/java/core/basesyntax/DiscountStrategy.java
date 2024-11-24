147459f68a80c767a969e76f81506ced5605c26c
Teslenko Daniil <burgalhashem@gmail.com>
2024-11-24 14:01:09 +0200

    implements discount strategy

--- src/main/java/core/basesyntax/DiscountStrategy.java
+++ src/main/java/core/basesyntax/DiscountStrategy.java
@@ -7,7 +7,7 @@ import core.basesyntax.strategy.impl.DefaultDiscountService;
 import core.basesyntax.strategy.impl.NewYearDiscountService;
 
 public class DiscountStrategy {
-   public DiscountService getDiscountServiceBySpecialEvent(String specialEvent) {
+    public DiscountService getDiscountServiceBySpecialEvent(String specialEvent) {
         switch (specialEvent) {
             case "Birthday":
                 return new BirthdayDiscountService();
