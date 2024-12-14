56f6085158a750ea4d8868d48ea270e854cc8c60
Rafal <pyszkowski.r@gmail.com>
2024-12-14 18:57:02 +0100

    done2

--- src/main/java/core/basesyntax/main.java
+++ src/main/java/core/basesyntax/main.java
@@ -1,14 +1,13 @@
 package core.basesyntax;
 
-import core.basesyntax.strategy.DiscountService;
 
 public class main {
 
-        public static void main(String[] args) {
-            String specialEvent = "New Year";
-            DiscountStrategy discountStrategy = new DiscountStrategy();
-            double discountService = discountStrategy
-                    .getDiscountServiceBySpecialEvent(specialEvent).getDiscount();
-            System.out.println(discountService);
-        }
+    public static void main(String[] args) {
+        String specialEvent = "New Year";
+        DiscountStrategy discountStrategy = new DiscountStrategy();
+        double discountService = discountStrategy
+                .getDiscountServiceBySpecialEvent(specialEvent).getDiscount();
+        System.out.println(discountService);
+    }
 }
