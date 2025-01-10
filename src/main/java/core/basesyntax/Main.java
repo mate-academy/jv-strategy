f6ee0bad4b7b86eeddfe13e974692890bb535522
Maxim Golyak <maximgoliak@outlook.com>
2025-01-10 15:16:30 +0200

    added getDiscount

---
+++ src/main/java/core/basesyntax/Main.java
@@ -0,0 +1,13 @@
+package core.basesyntax;
+
+public class Main {
+    public static void main(String[] args) {
+        /*System.out.println("+++");
+
+        DiscountStrategy discountStrategy = new DiscountStrategy();
+        DiscountService discountService = discountStrategy
+                .getDiscountServiceBySpecialEvent("birthday");
+        double discount = discountService.getDiscount();
+        System.out.println(discount);*/
+    }
+}
--- src/main/java/core/basesyntax/strategy/DiscountService.java
+++ src/main/java/core/basesyntax/strategy/DiscountService.java
@@ -2,4 +2,5 @@ package core.basesyntax.strategy;
 
 public interface DiscountService {
     double getDiscount();
+
 }
