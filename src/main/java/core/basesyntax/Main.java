5ca95d672d47cb7ee6f4fc951ab6f5f1d0760cfa
Povch Anastasia <povkhnastiya@gmail.com>
2025-01-24 01:53:31 +0200

    Procent price

---
+++ src/main/java/core/basesyntax/Main.java
@@ -0,0 +1,27 @@
+package core.basesyntax;
+
+import core.basesyntax.strategy.DiscountService;
+import core.basesyntax.strategy.DiscountStrategy;
+
+/**
+ * Feel free to remove this class and create your own.
+ */
+public class Main {
+    public static void main(String[] args) {
+        DiscountStrategy discountStrategy = new DiscountStrategy();
+
+        DiscountService newYearDiscount = discountStrategy.getDiscountServiceBySpecialEvent("New Year");
+        System.out.println("New Year Discount: " + newYearDiscount.getDiscount() + "%");
+
+        DiscountService birthdayDiscount = discountStrategy.getDiscountServiceBySpecialEvent("Birthday");
+        System.out.println("Birthday Discount: " + birthdayDiscount.getDiscount() + "%");
+
+        DiscountService blackFridayDiscount = discountStrategy.getDiscountServiceBySpecialEvent("Black Friday");
+        System.out.println("Black Friday Discount: " + blackFridayDiscount.getDiscount() + "%");
+
+        DiscountService defaultDiscount = discountStrategy.getDiscountServiceBySpecialEvent("Unknown Event");
+        System.out.println("Default Discount: " + defaultDiscount.getDiscount() + "%");
+
+    }
+
+}
