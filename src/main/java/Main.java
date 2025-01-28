dcff87460d214252664c30707c64fe14b3f87953
Kostyantyn Savchenko <kostyantyn.savchenko@gmail.com>
2025-01-28 20:42:24 +0200

    hw-jv-strategy solution v1.1

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+import core.basesyntax.DiscountStrategy;
+import core.basesyntax.strategy.DiscountService;
+
+public class Main {
+    public static void main(String[] args) {
+        DiscountStrategy strategy = new DiscountStrategy();
+        DiscountService discountService = strategy.getDiscountServiceBySpecialEvent("New Year");
+        double discount = discountService.getDiscount();
+        System.out.println(discount);
+    }
+}
