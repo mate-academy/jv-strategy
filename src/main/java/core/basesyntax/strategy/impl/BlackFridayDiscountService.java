8c531521f3cdc86485f97c7fc86ca0031c40f0fb
Hanna Voloshko <aa.voloshko09@gmail.com>
2025-01-30 21:38:56 +0200

    implemented all logic. all tests passed

--- src/main/java/core/basesyntax/strategy/impl/BlackFridayDiscountService.java
+++ src/main/java/core/basesyntax/strategy/impl/BlackFridayDiscountService.java
@@ -5,6 +5,6 @@ import core.basesyntax.strategy.DiscountService;
 public class BlackFridayDiscountService implements DiscountService {
     @Override
     public double getDiscount() {
-        return 0.45;
+        return 45.0;
     }
 }
