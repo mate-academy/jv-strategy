f5efb34f32f3ecdce70b35deee5ca67cbd47693e
Mateusz <mateusz.kiryk@o2.pl>
2025-02-17 21:50:15 +0100

    Second solution

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -7,15 +7,19 @@ public class Main {
         DiscountStrategy discountStrategy = new DiscountStrategy();
 
         DiscountService discountService = discountStrategy.getDiscountServiceBySpecialEvent("New Year");
-        System.out.println("New Year Discount: " + discountService.getDiscount() + "%");
+        System.out.println("New Year Discount: "
+                + discountService.getDiscount() + "%");
 
         discountService = discountStrategy.getDiscountServiceBySpecialEvent("Birthday");
-        System.out.println("Birthday Discount: " + discountService.getDiscount() + "%");
+        System.out.println("Birthday Discount: "
+                + discountService.getDiscount() + "%");
 
         discountService = discountStrategy.getDiscountServiceBySpecialEvent("Black Friday");
-        System.out.println("Black Friday Discount: " + discountService.getDiscount() + "%");
+        System.out.println("Black Friday Discount: "
+                + discountService.getDiscount() + "%");
 
         discountService = discountStrategy.getDiscountServiceBySpecialEvent("Holiday");
-        System.out.println("Holiday Discount (Default): " + discountService.getDiscount() + "%");
+        System.out.println("Holiday Discount (Default): "
+                + discountService.getDiscount() + "%");
     }
 }
