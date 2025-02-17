8b2115181776b269688230af6ba6442052fe44ad
Mateusz <mateusz.kiryk@o2.pl>
2025-02-17 21:53:11 +0100

    Third solution

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -4,21 +4,26 @@ import core.basesyntax.strategy.DiscountService;
 
 public class Main {
     public static void main(String[] args) {
-        DiscountStrategy discountStrategy = new DiscountStrategy();
+        DiscountStrategy discountStrategy
+                = new DiscountStrategy();
 
-        DiscountService discountService = discountStrategy.getDiscountServiceBySpecialEvent("New Year");
+        DiscountService discountService
+                = discountStrategy.getDiscountServiceBySpecialEvent("New Year");
         System.out.println("New Year Discount: "
                 + discountService.getDiscount() + "%");
 
-        discountService = discountStrategy.getDiscountServiceBySpecialEvent("Birthday");
+        discountService
+                = discountStrategy.getDiscountServiceBySpecialEvent("Birthday");
         System.out.println("Birthday Discount: "
                 + discountService.getDiscount() + "%");
 
-        discountService = discountStrategy.getDiscountServiceBySpecialEvent("Black Friday");
+        discountService
+                = discountStrategy.getDiscountServiceBySpecialEvent("Black Friday");
         System.out.println("Black Friday Discount: "
                 + discountService.getDiscount() + "%");
 
-        discountService = discountStrategy.getDiscountServiceBySpecialEvent("Holiday");
+        discountService
+                = discountStrategy.getDiscountServiceBySpecialEvent("Holiday");
         System.out.println("Holiday Discount (Default): "
                 + discountService.getDiscount() + "%");
     }
