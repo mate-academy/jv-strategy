9ba8ab9e124880e6546351896ad72cd79f0b44a8
Weronika Rzekiecka <weronikarzekiecka@interia.pl>
2025-02-18 12:59:45 +0100

    poprawka

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -1,15 +1,13 @@
 package core.basesyntax;
 
-
-
-
 import core.basesyntax.strategy.DiscountService;
 
 public class HelloWorld {
     public static void main(String[] args) {
         DiscountStrategy discountStrategy = new DiscountStrategy();
-        DiscountService discountService = discountStrategy.getDiscountServiceBySpecialEvent("New Year");
-        System.out.println("Discount: " + discountService.getDiscount() + "%");
+        DiscountService discountService =
+                discountStrategy.getDiscountServiceBySpecialEvent("New Year");
+        System.out.println("Discount: "
+                + discountService.getDiscount() + "%");
     }
-
 }
