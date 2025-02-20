7ea1680a8636c36c067cf7afb088ec55187586a8
Weronika Rzekiecka <weronikarzekiecka@interia.pl>
2025-02-20 15:09:48 +0100

    solution

--- src/main/java/core/basesyntax/DiscountStrategy.java
+++ src/main/java/core/basesyntax/DiscountStrategy.java
@@ -1,13 +1,17 @@
 package core.basesyntax;
 
 import core.basesyntax.strategy.DiscountService;
-import core.basesyntax.strategy.impl.DefaultDiscountService;
 import core.basesyntax.strategy.impl.BirthdayDiscountService;
 import core.basesyntax.strategy.impl.BlackFridayDiscountService;
+import core.basesyntax.strategy.impl.DefaultDiscountService;
 import core.basesyntax.strategy.impl.NewYearDiscountService;
 
 public class DiscountStrategy {
     public DiscountService getDiscountServiceBySpecialEvent(String specialEvent) {
+        if (specialEvent == null) {
+            return new DefaultDiscountService();
+        }
+
         switch (specialEvent) {
             case "New Year":
                 return new NewYearDiscountService();
@@ -19,5 +23,4 @@ public class DiscountStrategy {
                 return new DefaultDiscountService();
         }
     }
-
 }
--- src/main/java/core/basesyntax/strategy/impl/NewYearDiscountService.java
+++ src/main/java/core/basesyntax/strategy/impl/NewYearDiscountService.java
@@ -3,9 +3,9 @@ package core.basesyntax.strategy.impl;
 import core.basesyntax.strategy.DiscountService;
 
 public class NewYearDiscountService implements DiscountService {
+
     @Override
     public double getDiscount() {
         return 20;
     }
-
 }
