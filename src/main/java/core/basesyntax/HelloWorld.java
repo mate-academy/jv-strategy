1abe510ea4514d3345eb5d852800cf8ec0048c6a
Weronika Rzekiecka <weronikarzekiecka@interia.pl>
2025-02-25 12:52:09 +0100

    strategy

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -1,8 +1,13 @@
 package core.basesyntax;
 
-/**
- * Feel free to remove this class and create your own.
- */
-public class HelloWorld {
+import core.basesyntax.strategy.DiscountService;
 
+public class HelloWorld {
+    public static void main(String[] args) {
+        DiscountStrategy discountStrategy = new DiscountStrategy();
+        DiscountService discountService =
+                discountStrategy.getDiscountServiceBySpecialEvent("New Year");
+        System.out.println("Discount: "
+                + discountService.getDiscount() + "%");
+    }
 }
