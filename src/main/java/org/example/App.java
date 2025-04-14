ac8808d4700d268aca76d865e85e584a6c2406bf
Iryna Kravchenko <kravchenkoiryna1207@gmail.com>
2025-04-14 20:54:29 +0200

    Changed 'App' style

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,10 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
+public class App
 {
-    public static void main( String[] args )
+    public static void main(String[] args)
     {
-        System.out.println( "Hello World!" );
+        System.out.println("Hello World!");
     }
 }
+
