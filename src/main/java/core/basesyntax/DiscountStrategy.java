e45cb33d851b1d416591fe1687ff6c5e841ac6fe
DenysHirniak <denforever777@gmail.com>
2025-09-18 14:42:44 +0300

    remove redundant imports

--- src/main/java/core/basesyntax/DiscountStrategy.java
+++ src/main/java/core/basesyntax/DiscountStrategy.java
@@ -1,6 +1,5 @@
 package core.basesyntax;
 
-import core.basesyntax.DiscountService;
 import core.basesyntax.strategy.impl.BirthdayDiscountService;
 import core.basesyntax.strategy.impl.BlackFridayDiscountService;
 import core.basesyntax.strategy.impl.DefaultDiscountService;
