e508fb00af181966a25a9381685c1c19a13a1e58
AndreaTkachuk <kovklad@gmail.com>
2025-09-28 21:06:10 +0200

    solution2

--- src/main/java/core/basesyntax/strategy/impl/DefaultDiscountService.java
+++ src/main/java/core/basesyntax/strategy/impl/DefaultDiscountService.java
@@ -3,8 +3,8 @@ package core.basesyntax.strategy.impl;
 import core.basesyntax.strategy.DiscountService;
 
 public class DefaultDiscountService implements DiscountService {
-  @Override
-  public double getDiscount() {
-    return 0;
-  }
+    @Override
+    public double getDiscount() {
+        return 0;
+    }
 }
--- src/main/java/core/basesyntax/strategy/impl/NewYearDiscountService.java
+++ src/main/java/core/basesyntax/strategy/impl/NewYearDiscountService.java
@@ -3,8 +3,8 @@ package core.basesyntax.strategy.impl;
 import core.basesyntax.strategy.DiscountService;
 
 public class NewYearDiscountService implements DiscountService {
-  @Override
-  public double getDiscount() {
-    return 20;
-  }
+    @Override
+    public double getDiscount() {
+        return 20;
+    }
 }
