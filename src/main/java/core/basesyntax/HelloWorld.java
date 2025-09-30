5544c46ff5d1f8fe99fb726d396dd4a2e5e5f77e
Krzysztof Piatkowski <krzysztofpiatkowski1987@gmail.com>
2025-09-30 12:21:59 +0200

    Task solution

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -1,8 +1,18 @@
 package core.basesyntax;
 
+import core.basesyntax.strategy.DiscountService;
+
 /**
  * Feel free to remove this class and create your own.
  */
 public class HelloWorld {
+
+    double price = 200.00;
+    DiscountStrategy strategy = new DiscountStrategy();
+
+    DiscountService service = strategy.getDiscountServiceBySpecialEvent("Black Friday");
+    double discountPercent = service.getDiscount();
+
+    double finalPrice = price * (1 - discountPercent / 100);
 
 }
