8ee882979c341607366c17a3ad6ea8aa754e667d
Krzysztof Piatkowski <krzysztofpiatkowski1987@gmail.com>
2025-09-30 12:29:30 +0200

    Add fixes

--- src/main/java/core/basesyntax/DiscountStrategy.java
+++ src/main/java/core/basesyntax/DiscountStrategy.java
@@ -7,8 +7,12 @@ import core.basesyntax.strategy.impl.DefaultDiscountService;
 import core.basesyntax.strategy.impl.NewYearDiscountService;
 
 public class DiscountStrategy {
+    private static final DiscountService DEFAULT = new DefaultDiscountService();
 
     public DiscountService getDiscountServiceBySpecialEvent(String specialEvent) {
+        if (specialEvent == null) {
+            return DEFAULT;
+        }
 
         switch (specialEvent) {
             case "Birthday":
@@ -18,7 +22,7 @@ public class DiscountStrategy {
             case "New Year":
                 return new NewYearDiscountService();
             default:
-                return new DefaultDiscountService();
+                return DEFAULT;
         }
     }
 }
