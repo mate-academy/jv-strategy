491f9c4beebcb8357ef39291baa777c3a2d9ec9d
Vitaliy Tishik <vitisik54@gmail.com>
2025-10-10 02:43:28 +0300

    Realize pattern Strategy

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -1,8 +1,21 @@
 package core.basesyntax;
 
-/**
- * Feel free to remove this class and create your own.
- */
-public class HelloWorld {
+import core.basesyntax.strategy.DiscountService;
 
+public class HelloWorld {
+    public static void main(String[] args) {
+        DiscountStrategy strategy = new DiscountStrategy();
+
+        DiscountService birthday = strategy.getDiscountServiceBySpecialEvent("Birthday");
+        System.out.println("Birthday discount: " + birthday.getDiscount());
+
+        DiscountService blackFriday = strategy.getDiscountServiceBySpecialEvent("Black Friday");
+        System.out.println("Black Friday discount: " + blackFriday.getDiscount());
+
+        DiscountService newYear = strategy.getDiscountServiceBySpecialEvent("New Year");
+        System.out.println("New Year discount: " + newYear.getDiscount());
+
+        DiscountService unknown = strategy.getDiscountServiceBySpecialEvent("SomeEvent");
+        System.out.println("Unknown event discount: " + unknown.getDiscount());
+    }
 }
