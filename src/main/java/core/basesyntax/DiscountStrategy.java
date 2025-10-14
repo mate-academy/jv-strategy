231f741900f85eea7219b4fb94f936fa3f0d1e88
midoridoriii <valissa069@gmail.com>
2025-10-14 15:33:42 +0300

    added classes according the task ang implemented discount strategy in each class

---
+++ src/main/java/core/basesyntax/DiscountStrategy.java
@@ -0,0 +1,21 @@
+package core.basesyntax;
+
+public class DiscountStrategy implements DiscountService {
+    public DiscountService getDiscountServiceBySpecialEvent(String specialEvent) {
+        switch (specialEvent) {
+            case "Birthday":
+                return new BirthdayDiscountService();
+            case "New Year":
+                return new NewYearDiscountService();
+            case "Black Friday":
+                return new BlackFridayDiscountService();
+            default:
+                return new DefaultDiscountService();
+        }
+    }
+
+    @Override
+    public double getDiscount() {
+        return 0;
+    }
+}
