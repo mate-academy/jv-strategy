6ed57310a657613c2899119a9962c2e879831fb3
Andrew <reznichenko.a200212@gmail.com>
2025-11-01 22:19:12 +0200

    try1

---
+++ src/main/java/core/basesyntax/DiscountStrategy.java
@@ -0,0 +1,9 @@
+package core.basesyntax;
+import core.basesyntax.strategy.DiscountService;
+import core.basesyntax.strategy.impl.*;
+
+public class DiscountStrategy {
+    public DiscountService getDiscountServiceBySpecialEvent(String specialEvent) {
+
+    }
+}
