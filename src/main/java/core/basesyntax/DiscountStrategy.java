1bc673ad0bec4d04d8c136507f8db2abae026ae5
Martyna Szałańska <szalanskatita2002@gmail.com>
2026-01-19 01:42:49 +0100

    Fixed problem with Packages

--- src/main/java/core/basesyntax/DiscountStrategy.java
+++ src/main/java/core/basesyntax/DiscountStrategy.java
@@ -1,9 +1,10 @@
 package core.basesyntax;
 
-import core.basesyntax.strategy.BirthdayDiscountService;
-import core.basesyntax.strategy.BlackFridayDiscountService;
-import core.basesyntax.strategy.DefaultDiscountService;
-import core.basesyntax.strategy.NewYearDiscountService;
+import core.basesyntax.strategy.*;
+import core.basesyntax.strategy.impl.BirthdayDiscountService;
+import core.basesyntax.strategy.impl.BlackFridayDiscountService;
+import core.basesyntax.strategy.impl.DefaultDiscountService;
+import core.basesyntax.strategy.impl.NewYearDiscountService;
 
 public class DiscountStrategy {
     public DiscountService getDiscountServiceBySpecialEvent(String specialEvent) {
--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -1,5 +1,7 @@
 package core.basesyntax;
 
+import core.basesyntax.strategy.DiscountService;
+
 /**
  * Feel free to remove this class and create your own.
  */
