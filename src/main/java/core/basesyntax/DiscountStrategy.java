f4b054f6de292c1b6cb361dffb6aa03bb38f10fa
michal <mihacik228@gmail.com>
2026-01-21 15:50:49 +0100

    task completed

--- src/main/java/core/basesyntax/DiscountStrategy.java
+++ src/main/java/core/basesyntax/DiscountStrategy.java
@@ -10,13 +10,13 @@ public class DiscountStrategy {
     public DiscountService getDiscountServiceBySpecialEvent(String specialEvent) {
         switch (specialEvent) {
             case "Birthday":
-                  return new BirthdayDiscountService();
+                return new BirthdayDiscountService();
             case "Black Friday":
                 return new BlackFridayDiscountService();
             case "New Year":
                 return new NewYearDiscountService();
             default:
-                  return new DefaultDiscountService();
+                return new DefaultDiscountService();
         }
     }
 }
