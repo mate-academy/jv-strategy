5906ce3808ad861e2904f2ae1919d068f8d833f1
Yehor Osadchi <osadcijyehor@gmail.com>
2026-01-26 12:27:57 +0200

    I did

---
+++ src/main/java/core/basesyntax/DiscountStrategy.java
@@ -0,0 +1,22 @@
+package core.basesyntax;
+
+import core.basesyntax.strategy.DiscountService;
+import core.basesyntax.strategy.impl.BirthdayDiscountService;
+import core.basesyntax.strategy.impl.BlackFridayDiscountService;
+import core.basesyntax.strategy.impl.DefaultDiscountService;
+import core.basesyntax.strategy.impl.NewYearDiscountService;
+
+public class DiscountStrategy {
+    public DiscountService getDiscountServiceBySpecialEvent(final String specialEvent) {
+        if ("Birthday".equals(specialEvent)) {
+            return new BirthdayDiscountService();
+        }
+        if ("Black Friday".equals(specialEvent)) {
+            return new BlackFridayDiscountService();
+        }
+        if ("New Year".equals(specialEvent)) {
+            return new NewYearDiscountService();
+        }
+        return new DefaultDiscountService();
+    }
+}
