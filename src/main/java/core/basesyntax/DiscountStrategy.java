57cc0c6196a639682a750a88e40c8908ba6c98f3
Timofii <regen69neger@gmail.com>
2026-01-29 21:33:26 +0200

    changed maven compile version

--- src/main/java/core/basesyntax/DiscountStrategy.java
+++ src/main/java/core/basesyntax/DiscountStrategy.java
@@ -6,7 +6,7 @@ import core.basesyntax.strategy.impl.BlackFridayDiscountService;
 import core.basesyntax.strategy.impl.DefaultDiscountService;
 import core.basesyntax.strategy.impl.NewYearDiscountService;
 
-//Some changes for git
+//Some changes for git e
 
 public class DiscountStrategy {
     public DiscountService getDiscountServiceBySpecialEvent(String specialEvent) {
