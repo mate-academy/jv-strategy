9450ce964654c4e12b6fb62e328989062513471e
MrRave <lundgren.play@gmail.com>
2026-02-26 18:00:02 +0200

    added logic for getting discound strategy

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -4,5 +4,15 @@ package core.basesyntax;
  * Feel free to remove this class and create your own.
  */
 public class HelloWorld {
-
+    public static void main(String[] args) {
+        DiscountStrategy discountStrategy = new DiscountStrategy();
+        System.out.println(discountStrategy
+                .getDiscountServiceBySpecialEvent("Birthday").getDiscount());
+        System.out.println(discountStrategy
+                .getDiscountServiceBySpecialEvent("Black Friday").getDiscount());
+        System.out.println(discountStrategy
+                .getDiscountServiceBySpecialEvent("New Year").getDiscount());
+        System.out.println(discountStrategy
+                .getDiscountServiceBySpecialEvent("Default").getDiscount());
+    }
 }
