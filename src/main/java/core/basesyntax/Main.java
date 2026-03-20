33487a3ee2bf5eb5af6651a0a0182b447ff6fe09
Brynkiewicz <michal.brynkiewicz@envistaco.com>
2026-03-20 19:59:21 +0100

    DiscountStrategy - first solution

---
+++ src/main/java/core/basesyntax/Main.java
@@ -0,0 +1,12 @@
+package core.basesyntax;
+
+import core.basesyntax.strategy.DiscountService;
+
+public class Main {
+    public static void main(String[] args) {
+        String specialEvent = "Birthday";
+        DiscountStrategy discountStrategy = new DiscountStrategy();
+        DiscountService discountService = discountStrategy.getDiscountServiceBySpecialEvent(specialEvent);
+        System.out.println(discountService.getDiscount());
+    }
+}
