a4d10d402aeacde380d806ceba89dbeacd49e11c
Mariia Makohin <maria.makohin2420@gmail.com>
2026-05-17 10:34:38 +0300

    final changes

--- src/main/java/core/basesyntax/DiscountStrategy.java
+++ src/main/java/core/basesyntax/DiscountStrategy.java
@@ -3,8 +3,8 @@ package core.basesyntax;
 import core.basesyntax.strategy.DiscountService;
 import core.basesyntax.strategy.impl.BirthdayDiscountService;
 import core.basesyntax.strategy.impl.BlackFridayDiscountService;
-import core.basesyntax.strategy.impl.NewYearDiscountService;
 import core.basesyntax.strategy.impl.DefaultDiscountService;
+import core.basesyntax.strategy.impl.NewYearDiscountService;
 
 public class DiscountStrategy {
     public DiscountService getDiscountServiceBySpecialEvent(String specialEvent) {
